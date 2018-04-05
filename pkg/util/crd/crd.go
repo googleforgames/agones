62ef01a6b8c2f79d17b2d5e0f6187b5d1e97eecb
Davor Kapsa <davor.kapsa@gmail.com>
2018-04-05 23:10:10 +0200

    fix crd package comment

--- pkg/util/crd/crd.go
+++ pkg/util/crd/crd.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// package crd contains utilities for working with
-// CustomResourceDefinitions
+// Package crd contains utilities for working with
+// CustomResourceDefinitions.
 package crd
 
 import (
