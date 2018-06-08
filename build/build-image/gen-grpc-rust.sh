1766eaadf0acfdd321b5e8570bd79fb69c06a979
Mark Mandel <markmandel@google.com>
2018-06-08 11:44:09 -0700

    Add Licences to Rust SDK & Examples
    Realised that all the new Rust SDK code
    didn't have the Apache licence applied.
    
    This applies the licence.

--- build/build-image/gen-grpc-rust.sh
+++ build/build-image/gen-grpc-rust.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 cd /go/src/agones.dev/agones
 protoc --rust_out sdks/rust/src/grpc --grpc_out=sdks/rust/src/grpc --plugin=protoc-gen-grpc=`which grpc_rust_plugin` sdk.proto
+
+cat ./build/boilerplate.go.txt ./sdks/rust/src/grpc/sdk.rs >> ./sdk.rs
+cat ./build/boilerplate.go.txt ./sdks/rust/src/grpc/sdk_grpc.rs >> ./sdk_grpc.rs
+mv ./sdk.rs ./sdks/rust/src/grpc/
+mv ./sdk_grpc.rs ./sdks/rust/src/grpc/
