86240b1713d124aceb8c7d5f3f0d6b7a4dbfff89
Mark Mandel <markmandel@google.com>
2018-06-08 22:17:53 -0700

    Upgrade gRPC to 1.12.0
    This also replaces the C++ build image we have from the gRPC docker hub,
    with one that we manage ourselves, since getting updates in a timely
    matter was getting difficult.
    
    The C++ base will likely go away once the transition to REST based SDK,
    so the base may end up going away.
    
    This is prerequisite for #240

--- sdks/cpp/sdk.h
+++ sdks/cpp/sdk.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <grpc++/grpc++.h>
+#include <grpcpp/grpcpp.h>
 #include "sdk.grpc.pb.h"
 
 namespace agones {
