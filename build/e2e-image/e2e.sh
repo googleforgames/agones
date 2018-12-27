051d9fb38137108a8bf4b835fcc92c8892052823
Mark Mandel <markmandel@google.com>
2018-12-27 15:29:10 +1100

    This should fail e2e in any command fails
    In the current incarnation, if the install section
    fails, and e2e passes, then e2e all passes.
    
    This should fail now if install fails.

--- build/e2e-image/e2e.sh
+++ build/e2e-image/e2e.sh
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -e
 echo "installing current release"
 DOCKER_RUN= make install
 echo "starting e2e test"
