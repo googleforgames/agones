047b23e7f7440b4df301b58ead39546fa827134e
Mark Mandel <markmandel@google.com>
2019-02-21 19:31:56 +1100

    Organising Makefile into includes
    Just moving some of the less "main" pieces into includes,
    so the build/Makefile isn't quite so unweildy.
    
    Something I've wanted to do for a while.

---
+++ build/includes/google-cloud.mk
@@ -0,0 +1,77 @@
+# Copyright 2019 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#    ____                   _         ____ _                 _
+#   / ___| ___   ___   __ _| | ___   / ___| | ___  _   _  __| |
+#  | |  _ / _ \ / _ \ / _` | |/ _ \ | |   | |/ _ \| | | |/ _` |
+#  | |_| | (_) | (_) | (_| | |  __/ | |___| | (_) | |_| | (_| |
+#   \____|\___/ \___/ \__, |_|\___|  \____|_|\___/ \__,_|\__,_|
+#                     |___/
+
+# Initialise the gcloud login and project configuration, if you are working with GCP
+gcloud-init: ensure-build-config
+	docker run --rm -it $(common_mounts) $(build_tag) gcloud init
+
+# Creates and authenticates a small, 6 node GKE cluster to work against (2 nodes are used for agones-metrics and agones-system)
+gcloud-test-cluster: GCP_CLUSTER_LEGACYABAC ?= false
+gcloud-test-cluster: GCP_CLUSTER_NODEPOOL_INITIALNODECOUNT ?= 4
+gcloud-test-cluster: GCP_CLUSTER_NODEPOOL_MACHINETYPE ?= n1-standard-4
+gcloud-test-cluster: $(ensure-build-image)
+	docker run --rm -it $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) gcloud \
+		deployment-manager deployments create $(GCP_CLUSTER_NAME)  \
+		--properties cluster.zone:$(GCP_CLUSTER_ZONE),cluster.name:$(GCP_CLUSTER_NAME),cluster.nodePool.initialNodeCount:$(GCP_CLUSTER_NODEPOOL_INITIALNODECOUNT),cluster.nodePool.machineType:$(GCP_CLUSTER_NODEPOOL_MACHINETYPE),cluster.legacyAbac:$(GCP_CLUSTER_LEGACYABAC)\
+		--template=$(mount_path)/build/gke-test-cluster/cluster.yml.jinja
+	$(MAKE) gcloud-auth-cluster
+	$(MAKE) setup-test-cluster
+
+clean-gcloud-test-cluster: $(ensure-build-image)
+	docker run --rm -it $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) gcloud \
+		deployment-manager deployments delete $(GCP_CLUSTER_NAME)
+
+# Creates a gcloud cluster for end-to-end
+# it installs also a consul cluster to handle build system concurrency using a distributed lock
+gcloud-e2e-test-cluster: $(ensure-build-image)
+	docker run --rm -it $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) gcloud \
+		deployment-manager deployments create e2e-test-cluster \
+		--config=$(mount_path)/build/gke-test-cluster/cluster-e2e.yml
+	GCP_CLUSTER_NAME=e2e-test-cluster GCP_CLUSTER_ZONE=us-west1-c $(MAKE) gcloud-auth-cluster
+	docker run --rm $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) \
+		kubectl apply -f $(mount_path)/build/helm.yaml
+	docker run --rm $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) \
+		helm init --service-account helm --wait
+	docker run --rm $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) \
+		helm install --wait --set Replicas=1,uiService.type=ClusterIP --name consul stable/consul
+
+# Deletes the gcloud e2e cluster and cleanup any left pvc volumes
+clean-gcloud-e2e-test-cluster: $(ensure-build-image)
+	docker run --rm $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) \
+		helm delete --purge consul && kubectl delete pvc -l component=consul-consul
+	GCP_CLUSTER_NAME=e2e-test-cluster $(MAKE) clean-gcloud-test-cluster
+
+# Pulls down authentication information for kubectl against a cluster, name can be specified through GCP_CLUSTER_NAME
+# (defaults to 'test-cluster')
+gcloud-auth-cluster: $(ensure-build-image)
+	docker run --rm $(common_mounts) $(build_tag) gcloud config set container/cluster $(GCP_CLUSTER_NAME)
+	docker run --rm $(common_mounts) $(build_tag) gcloud config set compute/zone $(GCP_CLUSTER_ZONE)
+	docker run --rm $(common_mounts) $(build_tag) gcloud container clusters get-credentials $(GCP_CLUSTER_NAME)
+	-docker run --rm $(common_mounts) $(build_tag) bash -c 'kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $$(gcloud config get-value account)'
+
+# authenticate our docker configuration so that you can do a docker push directly
+# to the gcr.io repository
+gcloud-auth-docker: $(ensure-build-image)
+	docker run --rm $(common_mounts) $(build_tag) gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io
+
+# Clean the gcloud configuration
+clean-gcloud-config:
+	-sudo rm -r $(build_path)/.config
---
+++ build/includes/minikube.mk
@@ -0,0 +1,109 @@
+# Copyright 2019 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+#   __  __ _       _ _          _
+#  |  \/  (_)_ __ (_) | ___   _| |__   ___
+#  | |\/| | | '_ \| | |/ / | | | '_ \ / _ \
+#  | |  | | | | | | |   <| |_| | |_) |  __/
+#  |_|  |_|_|_| |_|_|_|\_\\__,_|_.__/ \___|
+#
+
+# Switches to an "agones" profile, and starts a kubernetes cluster
+# of the right version.
+#
+# Use MINIKUBE_DRIVER variable to change the VM driver
+# (defaults virtualbox for Linux and macOS, hyperv for windows) if you so desire.
+minikube-test-cluster: DOCKER_RUN_ARGS+=--network=host -v $(minikube_cert_mount)
+minikube-test-cluster: $(ensure-build-image) minikube-agones-profile
+	# localkube bootstrapper fixes issues with profiles
+	$(MINIKUBE) start --kubernetes-version v1.11.5 --vm-driver $(MINIKUBE_DRIVER) \
+		--extra-config=apiserver.authorization-mode=RBAC
+	# wait until the master is up
+	until docker run --rm $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) kubectl cluster-info; \
+		do \
+			echo "Waiting for cluster to start..."; \
+			sleep 1; \
+		done
+	# this is needed for kubernetes component to work correctly while RBAC is enabled
+	-docker run --rm $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) \
+		kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --serviceaccount=kube-system:default
+	$(MAKE) setup-test-cluster DOCKER_RUN_ARGS="$(DOCKER_RUN_ARGS)"
+	$(MAKE) minikube-post-start
+
+# switch to the agones cluster
+minikube-agones-profile:
+	$(MINIKUBE) profile $(MINIKUBE_PROFILE)
+
+# Connecting to minikube requires so enhanced permissions, so use this target
+# instead of `make shell` to start an interactive shell for development on minikube.
+minikube-shell: $(ensure-build-image) minikube-agones-profile
+	$(MAKE) shell DOCKER_RUN_ARGS="--network=host -v $(minikube_cert_mount) $(DOCKER_RUN_ARGS)"
+
+# Push the local Agones Docker images that have already been built
+# via `make build` or `make build-images` into the "agones" minikube instance.
+minikube-push: minikube-agones-profile
+	$(MAKE) minikube-transfer-image TAG=$(sidecar_tag)
+	$(MAKE) minikube-transfer-image TAG=$(controller_tag)
+	$(MAKE) minikube-transfer-image TAG=$(ping_tag)
+
+# Installs the current development version of Agones into the Kubernetes cluster.
+# Use this instead of `make install`, as it disables PullAlways on the install.yaml
+minikube-install: minikube-agones-profile
+	$(MAKE) install DOCKER_RUN_ARGS="--network=host -v $(minikube_cert_mount)" ALWAYS_PULL_SIDECAR=false \
+		IMAGE_PULL_POLICY=IfNotPresent PING_SERVICE_TYPE=NodePort
+
+minikube-uninstall: $(ensure-build-image) minikube-agones-profile
+	$(MAKE) uninstall DOCKER_RUN_ARGS="--network=host -v $(minikube_cert_mount)"
+
+# Convenience target for transferring images into minikube.
+# Use TAG to specify the image to transfer into minikube
+minikube-transfer-image:
+	docker save $(TAG) | ($(MINIKUBE_DOCKER_ENV) && docker load)
+
+# Runs e2e tests against our minikube
+minikube-test-e2e: DOCKER_RUN_ARGS=--network=host -v $(minikube_cert_mount)
+minikube-test-e2e: minikube-agones-profile test-e2e
+
+# Runs stress tests against our minikube
+minikube-stress-test-e2e: DOCKER_RUN_ARGS=--network=host -v $(minikube_cert_mount)
+minikube-stress-test-e2e: minikube-agones-profile stress-test-e2e
+
+# prometheus on minkube
+# we have to disable PVC as it's not supported on minkube.
+minikube-setup-prometheus:
+	$(MAKE) setup-prometheus \
+		DOCKER_RUN_ARGS="--network=host -v $(minikube_cert_mount)" \
+		PVC=false HELM_ARGS="--set server.resources.requests.cpu=0,server.resources.requests.memory=0"
+
+
+# grafana on minkube with dashboards and prometheus datasource installed.
+# we have to disable PVC as it's not supported on minkube.
+minikube-setup-grafana:
+	$(MAKE) setup-grafana \
+		DOCKER_RUN_ARGS="--network=host -v $(minikube_cert_mount)"
+
+# minikube port forwarding
+minikube-controller-portforward:
+	$(MAKE) controller-portforward DOCKER_RUN_ARGS="--network=host -v $(minikube_cert_mount)"
+
+# minikube port forwarding grafana
+minikube-grafana-portforward:
+	$(MAKE) grafana-portforward \
+		DOCKER_RUN_ARGS="--network=host -v $(minikube_cert_mount)"
+
+# minikube port forwarding for prometheus web ui
+minikube-prometheus-portforward:
+	$(MAKE) prometheus-portforward \
+		DOCKER_RUN_ARGS="--network=host -v $(minikube_cert_mount)"
