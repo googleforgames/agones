902ed198f78e1bcaf0577745c302382486246a42
Alexander Apalikov <alexander.apalikov@globant.com>
2019-04-24 11:30:01 +0300

    Move terraform targets into a separate file
    Fix and return back setup-test-cluster step.
    Removed this step by mistake.

--- build/includes/google-cloud.mk
+++ build/includes/google-cloud.mk
@@ -33,70 +33,11 @@ gcloud-test-cluster: $(ensure-build-image)
 		--properties cluster.zone:$(GCP_CLUSTER_ZONE),cluster.name:$(GCP_CLUSTER_NAME),cluster.nodePool.initialNodeCount:$(GCP_CLUSTER_NODEPOOL_INITIALNODECOUNT),cluster.nodePool.machineType:$(GCP_CLUSTER_NODEPOOL_MACHINETYPE),cluster.legacyAbac:$(GCP_CLUSTER_LEGACYABAC)\
 		--template=$(mount_path)/build/gke-test-cluster/cluster.yml.jinja
 	$(MAKE) gcloud-auth-cluster
+	$(MAKE) setup-test-cluster
 
 clean-gcloud-test-cluster: $(ensure-build-image)
 	docker run --rm -it $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) gcloud \
 		deployment-manager deployments delete $(GCP_CLUSTER_NAME)
-
-### Deploy cluster with Terraform
-terraform-init:
-	docker run --rm -it $(common_mounts) $(DOCKER_RUN_ARGS) $(build_tag) bash -c '\
-	cd $(mount_path)/build && terraform init && gcloud auth application-default login'
-
-terraform-clean:
-	rm -r ./.terraform
-	rm ./terraform.tfstate*
-
-# Creates a cluster and install release version of Agones controller
-# Version could be specified by AGONES_VERSION
-gcloud-terraform-cluster: GCP_CLUSTER_LEGACYABAC ?= false
-gcloud-terraform-cluster: GCP_CLUSTER_NODEPOOL_INITIALNODECOUNT ?= 4
-gcloud-terraform-cluster: GCP_CLUSTER_NODEPOOL_MACHINETYPE ?= n1-standard-4
-gcloud-terraform-cluster: AGONES_VERSION ?= ''
-gcloud-terraform-cluster: $(ensure-build-image)
-gcloud-terraform-cluster:
-ifndef GCP_PROJECT
-	$(eval GCP_PROJECT=$(shell sh -c "gcloud config get-value project 2> /dev/null"))
-endif
-	$(DOCKER_RUN) bash -c 'export TF_VAR_agones_version=$(AGONES_VERSION) && \
-	    export TF_VAR_password=$(GKE_PASSWORD) && \
-		cd $(mount_path)/build && terraform apply -auto-approve -var values_file="" \
-		-var chart="agones" \
-	 	-var "cluster={name=\"$(GCP_CLUSTER_NAME)\", machineType=\"$(GCP_CLUSTER_NODEPOOL_MACHINETYPE)\", \
-		 zone=\"$(GCP_CLUSTER_ZONE)\", project=\"$(GCP_PROJECT)\", \
-		 initialNodeCount=\"$(GCP_CLUSTER_NODEPOOL_INITIALNODECOUNT)\", \
-		 legacyABAC=\"$(GCP_CLUSTER_LEGACYABAC)\"}"'
-	$(MAKE) gcloud-auth-cluster
-
-# Creates a cluster and install current version of Agones controller
-# Set all necessary variables as `make install` does
-gcloud-terraform-install: GCP_CLUSTER_LEGACYABAC ?= false
-gcloud-terraform-install: GCP_CLUSTER_NODEPOOL_INITIALNODECOUNT ?= 4
-gcloud-terraform-install: GCP_CLUSTER_NODEPOOL_MACHINETYPE ?= n1-standard-4
-gcloud-terraform-install: ALWAYS_PULL_SIDECAR := true
-gcloud-terraform-install: IMAGE_PULL_POLICY := "Always"
-gcloud-terraform-install: PING_SERVICE_TYPE := "LoadBalancer"
-gcloud-terraform-install: CRD_CLEANUP := true
-gcloud-terraform-install:
-ifndef GCP_PROJECT
-	$(eval GCP_PROJECT=$(shell sh -c "gcloud config get-value project 2> /dev/null"))
-endif
-	$(DOCKER_RUN) bash -c 'export TF_VAR_password=$(GKE_PASSWORD) && \
-		cd $(mount_path)/build && terraform apply -auto-approve -var agones_version="$(VERSION)" -var image_registry="$(REGISTRY)" \
-		-var pull_policy="$(IMAGE_PULL_POLICY)" \
-		-var always_pull_sidecar="$(ALWAYS_PULL_SIDECAR)" \
-		-var image_pull_secret="$(IMAGE_PULL_SECRET)" \
-		-var ping_service_type="$(PING_SERVICE_TYPE)" \
-		-var crd_cleanup="$(CRD_CLEANUP)" \
-		-var "cluster={name=\"$(GCP_CLUSTER_NAME)\", machineType=\"$(GCP_CLUSTER_NODEPOOL_MACHINETYPE)\", \
-		 zone=\"$(GCP_CLUSTER_ZONE)\", project=\"$(GCP_PROJECT)\", \
-		 initialNodeCount=\"$(GCP_CLUSTER_NODEPOOL_INITIALNODECOUNT)\", \
-		 legacyABAC=\"$(GCP_CLUSTER_LEGACYABAC)\"}"'
-	$(MAKE) gcloud-auth-cluster
-
-gcloud-terraform-destroy-cluster:
-	$(DOCKER_RUN) bash -c 'cd $(mount_path)/build && \
-	 terraform destroy -auto-approve'
 
 # Creates a gcloud cluster for end-to-end
 # it installs also a consul cluster to handle build system concurrency using a distributed lock
