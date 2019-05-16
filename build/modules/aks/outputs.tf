fe5450c91e4a3d0bf3014dad94e424a5be777698
Alexander Apalikov <alexander.apalikov@globant.com>
2019-05-16 08:53:47 +0300

    Add AKS, GKE and Helm terraform modules (#756)
    Add configuration for deploying cluster and installing Agones on AKS.

---
+++ build/modules/aks/outputs.tf
@@ -0,0 +1,34 @@
+# Copyright 2019 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+output "cluster_ca_certificate" {
+  value = "${base64decode(azurerm_kubernetes_cluster.test.kube_config.0.cluster_ca_certificate)}"
+}
+
+output "client_certificate" {
+  value = "${azurerm_kubernetes_cluster.test.kube_config.0.client_certificate}"
+}
+
+output "kube_config" {
+  value = "${azurerm_kubernetes_cluster.test.kube_config_raw}"
+}
+
+output "host" {
+  value = "${azurerm_kubernetes_cluster.test.kube_config.0.host}"
+}
+
+output "token" {
+  value = "${azurerm_kubernetes_cluster.test.kube_config.0.password}"
+}
+
