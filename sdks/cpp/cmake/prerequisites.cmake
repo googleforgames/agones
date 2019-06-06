43851cb785d9470b8d702a90ffebb073c8265b73
Dmitry Sazonov <dmitry.sazonov@globant.com>
2019-06-06 19:13:28 +0300

    Cpp SDK. Improved CMake build.

--- sdks/cpp/cmake/prerequisites.cmake
+++ sdks/cpp/cmake/prerequisites.cmake
@@ -14,16 +14,16 @@
 
 cmake_minimum_required (VERSION 3.13.0)
 
-option(BUILD_THIRDPARTY_DEBUG "Build debug version of thirdparty libraries (MSVC only)" TRUE)
+option(BUILD_THIRDPARTY_DEBUG "Build debug version of thirdparty libraries (MSVC only)" ON)
 set(OPENSSL_CONFIG_STRING "VC-WIN64A" CACHE STRING "See https://github.com/openssl/openssl/blob/master/INSTALL for details")
+set(THIRDPARTY_INSTALL_PATH "${CMAKE_BINARY_DIR}/third_party" CACHE STRING "Path for installing third-party OpenSSL and gRPC, if they are not found with find_package")
 
 if (NOT MSVC)
     set(BUILD_THIRDPARTY_DEBUG FALSE)
     set(OPENSSL_CONFIG_STRING "" CACHE STRING "" FORCE)
 endif()
 
-set(THIRDPARTY_INSTALL_PATH ${CMAKE_INSTALL_PREFIX})
-
+# gRPC repo and version
 set(gRPC_GIT_REPO "https://github.com/gRPC/gRPC.git")
 set(gRPC_GIT_TAG "v1.16.1")
 
@@ -34,16 +34,17 @@ set(OPENSSL_GIT_TAG "OpenSSL_1_1_1")
 include(FetchContent)
 
 function(download_git_repo NAME REPO TAG)
-    set(${NAME}_SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/${NAME}/src")
+    set(BASE_DIR ${CMAKE_CURRENT_BINARY_DIR}/${NAME})
+    set(${NAME}_SOURCE_DIR "${BASE_DIR}/src")
     FetchContent_Declare(
         ${NAME}
         GIT_REPOSITORY      "${REPO}"
         GIT_TAG             "${TAG}"
         PREFIX              ${NAME}
-        SOURCE_DIR          "${${NAME}_DIR}"
-        BINARY_DIR          "${NAME}/.bin"
-        INSTALL_DIR         "${NAME}/.install"
-        SUBBUILD_DIR        "${NAME}/.build"
+        SOURCE_DIR          "${BASE_DIR}/src"
+        BINARY_DIR          "${BASE_DIR}/.bin"
+        INSTALL_DIR         "${BASE_DIR}/.install"
+        SUBBUILD_DIR        "${BASE_DIR}/.build"
         CONFIGURE_COMMAND   ""
         BUILD_COMMAND       ""
         INSTALL_COMMAND     ""
@@ -58,10 +59,10 @@ function(download_git_repo NAME REPO TAG)
             GIT_REPOSITORY      "${REPO}"
             GIT_TAG             "${TAG}"
             PREFIX              ${NAME}
-            SOURCE_DIR          "${${NAME}_SOURCE_DIR}"
-            BINARY_DIR          "${NAME}/.bin"
-            INSTALL_DIR         "${NAME}/.install"
-            SUBBUILD_DIR        "${NAME}/.build"
+            SOURCE_DIR          "${BASE_DIR}/src"
+            BINARY_DIR          "${BASE_DIR}/.bin"
+            INSTALL_DIR         "${BASE_DIR}/.install"
+            SUBBUILD_DIR        "${BASE_DIR}/.build"
             CONFIGURE_COMMAND   ""
             BUILD_COMMAND       ""
             INSTALL_COMMAND     ""
@@ -91,8 +92,8 @@ function(invoke_cmake_build NAME CMAKELISTS_PATH)
     message(STATUS "Building ${NAME}...")
 
     # Build directory
-    set(BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/third_party/${NAME})
-    set(INSTALL_DIR ${THIRDPARTY_INSTALL_PATH})
+    set(BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/${NAME}/.bin)
+    set(INSTALL_DIR ${THIRDPARTY_INSTALL_PATH}/${NAME})
     file(MAKE_DIRECTORY ${BUILD_DIR})
     
     # Makefile generation
@@ -105,7 +106,7 @@ function(invoke_cmake_build NAME CMAKELISTS_PATH)
         set(ARG_CONFIG_RELEASE "")
     endif()
 
-    execute_and_check(${BUILD_DIR} ${CMAKE_COMMAND} ${CMAKELISTS_PATH} -G ${CMAKE_GENERATOR} -Wno-dev ${ARG_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${ARGN})
+    execute_and_check(${BUILD_DIR} ${CMAKE_COMMAND} ${CMAKELISTS_PATH} -G ${CMAKE_GENERATOR} -Wno-dev ${ARG_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_MODULE_PATH=${THIRDPARTY_INSTALL_PATH} -DCMAKE_PREFIX_PATH=${THIRDPARTY_INSTALL_PATH} ${ARGN})
 
     # Building
     if (BUILD_THIRDPARTY_DEBUG)
@@ -116,10 +117,11 @@ function(invoke_cmake_build NAME CMAKELISTS_PATH)
     set(${NAME}_DIR "${INSTALL_DIR}" PARENT_SCOPE)
 endfunction(invoke_cmake_build)
 
-# OpenSSL
+find_package(gRPC CONFIG QUIET)
 find_package(OpenSSL QUIET)
 
-if (NOT ${OpenSSL_FOUND})
+# OpenSSL // Required only for gRPC build. Do not build, if gRPC is found.
+if (NOT ${OpenSSL_FOUND} AND NOT ${gRPC_FOUND})
     set(OPENSSL_ROOT_DIR "${THIRDPARTY_INSTALL_PATH}/OpenSSL")
     find_package(OpenSSL QUIET)
     if (NOT ${OpenSSL_FOUND})
@@ -138,28 +140,34 @@ if (NOT ${OpenSSL_FOUND})
 endif()
 
 # gRPC
-find_package(gRPC QUIET)
-
 if (NOT ${gRPC_FOUND})
     download_git_repo(gRPC ${gRPC_GIT_REPO} ${gRPC_GIT_TAG})
+    file(MAKE_DIRECTORY ${THIRDPARTY_INSTALL_PATH})
+    set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${THIRDPARTY_INSTALL_PATH})
 
+    # Build gRPC prerequisites
     invoke_cmake_build(zlib ${gRPC_SOURCE_DIR}/third_party/zlib)
-    invoke_cmake_build(cares ${gRPC_SOURCE_DIR}/third_party/cares/cares)
+    invoke_cmake_build(c-ares ${gRPC_SOURCE_DIR}/third_party/cares/cares)
     invoke_cmake_build(Protobuf ${gRPC_SOURCE_DIR}/third_party/protobuf/cmake
         "-DZLIB_ROOT=${zlib_DIR}"
         "-Dprotobuf_MSVC_STATIC_RUNTIME=OFF"
         "-Dprotobuf_BUILD_TESTS=OFF"
     )
+    
+    # Build gRPC as cmake package
+    set(OPENSSL_PARAM "")
+    if (DEFINED OPENSSL_ROOT_DIR)
+        set(OPENSSL_PARAM "-DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}")
+    endif()
     invoke_cmake_build(gRPC ${gRPC_SOURCE_DIR}
-        "-DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}"
+        "${OPENSSL_PARAM}"
         "-DZLIB_ROOT=${zlib_DIR}"
         "-DgRPC_INSTALL=ON"
         "-DgRPC_BUILD_TESTS=OFF"
         "-DgRPC_PROTOBUF_PROVIDER=package"
+        "-DgRPC_PROTOBUF_PACKAGE_TYPE=CONFIG"
         "-DgRPC_ZLIB_PROVIDER=package"
         "-DgRPC_CARES_PROVIDER=package"
         "-DgRPC_SSL_PROVIDER=package"
     )
-    
-    set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${CMAKE_INSTALL_PREFIX})
 endif()
