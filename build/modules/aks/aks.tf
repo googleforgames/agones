f0ed4fbadfd7bf117eb7bf0717a1733933a606f5
Robert Bailey <robertbailey@google.com>
2019-11-06 22:15:46 -0800

    Update terraform configs to use Kubernetes 1.13.

--- build/modules/aks/aks.tf
+++ build/modules/aks/aks.tf
@@ -50,7 +50,7 @@ resource "azurerm_kubernetes_cluster" "test" {
   resource_group_name = "${azurerm_resource_group.test.name}"
   dns_prefix          = "agones"
 
-  kubernetes_version = "1.12.8"
+  kubernetes_version = "1.13.12"
 
   agent_pool_profile {
     name            = "default"
