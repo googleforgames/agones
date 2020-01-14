577b7e123ea1f4b34f26703fb776bdb2f1af9639
Alexander Apalikov <alexander.apalikov@globant.com>
2020-01-14 19:45:15 +0300

    Example fuzz Roundtrip test (#1269)
    Add Roundtrip tests for agones.dev external types.

---
+++ pkg/apis/agones/v1/fuzz_test/roundtrip_test.go
@@ -0,0 +1,32 @@
+package install
+
+import (
+	"math/rand"
+	"testing"
+
+	agonesv1 "agones.dev/agones/pkg/apis/agones/v1"
+	allocationv1 "agones.dev/agones/pkg/apis/allocation/v1"
+	autoscalingv1 "agones.dev/agones/pkg/apis/autoscaling/v1"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/api/apitesting/fuzzer"
+	"k8s.io/apimachinery/pkg/api/apitesting/roundtrip"
+	genericfuzzer "k8s.io/apimachinery/pkg/apis/meta/fuzzer"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+func TestRoundTripTypes(t *testing.T) {
+	scheme := runtime.NewScheme()
+	codecs := serializer.NewCodecFactory(scheme)
+
+	localSchemeBuilder := runtime.SchemeBuilder{
+		agonesv1.AddToScheme,
+		allocationv1.AddToScheme,
+		autoscalingv1.AddToScheme,
+	}
+	seed := rand.Int63()
+	localFuzzer := fuzzer.FuzzerFor(genericfuzzer.Funcs, rand.NewSource(seed), codecs)
+
+	assert.NoError(t, localSchemeBuilder.AddToScheme(scheme))
+	roundtrip.RoundTripExternalTypes(t, scheme, codecs, localFuzzer, nil)
+}
