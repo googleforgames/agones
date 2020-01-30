06a93b7d0db86e2b25b54edd9588af0aae3c2829
Wouter Verlaek <wouter@improbable.io>
2020-01-30 21:49:29 +0000

    Extend Agones Unreal SDK (#1303)
    * Add SetLabel SetAnnotation GetGameServer and request retries
    * Update Unreal sdk settings in docs
    * Fix docs list markdown
    * Add const and improve error message

--- sdks/unreal/Agones/Source/Agones/Model/GameServerSpec.h
+++ sdks/unreal/Agones/Source/Agones/Model/GameServerSpec.h
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC All Rights Reserved.
+// Copyright 2020 Google LLC All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,15 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "AgonesSettings.h"
+#pragma once
 
-UAgonesSettings::UAgonesSettings()
-	: Super()
-	, bHealthPingEnabled(true)
-	, HealthPingSeconds(5.0f)
-	, bDebugLogEnabled(false)
-	, RequestRetryLimit(30)
+#include "CoreMinimal.h"
+#include "JsonObject.h"
+#include "SpecHealth.h"
+#include "GameServerSpec.generated.h"
+
+USTRUCT()
+struct AGONES_API FSpec
 {
-}
+	GENERATED_BODY()
 
- 
+	UPROPERTY()
+	FHealth Health;
+
+	/** Default constructor, no initialization */
+	FSpec()
+	{}
+
+	FSpec(TSharedPtr<FJsonObject> JsonObject)
+	{
+		const TSharedPtr<FJsonObject>* HealthJsonObject;
+		if (JsonObject->TryGetObjectField(TEXT("health"), HealthJsonObject))
+		{
+			Health = FHealth(*HealthJsonObject);
+		}
+	}
+};
--- sdks/unreal/Agones/Source/Agones/Model/KeyValuePair.h
+++ sdks/unreal/Agones/Source/Agones/Model/KeyValuePair.h
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC All Rights Reserved.
+// Copyright 2020 Google LLC All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,15 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "AgonesSettings.h"
+#pragma once
 
-UAgonesSettings::UAgonesSettings()
-	: Super()
-	, bHealthPingEnabled(true)
-	, HealthPingSeconds(5.0f)
-	, bDebugLogEnabled(false)
-	, RequestRetryLimit(30)
+#include "CoreMinimal.h"
+#include "KeyValuePair.generated.h"
+
+USTRUCT()
+struct FKeyValuePair
 {
-}
+	GENERATED_BODY()
 
- 
+	UPROPERTY()
+	FString Key;
+
+	UPROPERTY()
+	FString Value;
+};
