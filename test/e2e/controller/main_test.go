b94b0f096d5e23a71cefe7e8776cf0b6954537c3
Mark Mandel <markmandel@google.com>
2020-02-08 00:04:47 -0800

    Flaky TestGameServerAllocationDeletionOnUnAllocate
    Realised that Go test will compile and run tests in parallel per file,
    which means we could shutdown the controller while an allocation is in
    process - which means, it will fail completely, as the Kubernetes API
    cannot reach the controller's http endpoint for allocation.
    
    Split the controller tests into a separate package which could be run
    with -parallel=1 so that there can be no race condition.
    
    Also did some grouping of e2e test functionality to reduce code
    repetition.
    
    Closes #1326

--- test/e2e/controller/main_test.go
+++ test/e2e/controller/main_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Google LLC All Rights Reserved.
+// Copyright 2020 Google LLC All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package e2e
+package controller
 
 import (
 	"log"
@@ -58,5 +58,4 @@ func TestMain(m *testing.M) {
 		os.Exit(exitCode)
 	}()
 	exitCode = m.Run()
-
 }
