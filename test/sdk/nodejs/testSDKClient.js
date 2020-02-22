46d69228bc555685860e863c1558255ecf47bcaa
Mark Mandel <markmandel@google.com>
2020-02-22 12:18:22 -0800

    Release NPM package, and Node update
    Made sense to do these two parts together, since they were intertwined.
    
    1. Closed #679 - create an interactive shell, so we can publish to
       @google-cloud/agones-sdk, since it requires some internal tools to
       make it happen.
    2. Also update the Node version to 12.x, since 11.x is deprecated
    3. Updated the image to use the new NPM package, and also while in there
       updated Node to 12.x and the Debian base to Buster. (Node image
       update for #1261)
    4. Updated the release checklist to use the interactive shell, rather
       than the `publish` command, which has now been removed.

--- test/sdk/nodejs/testSDKClient.js
+++ test/sdk/nodejs/testSDKClient.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const AgonesSDK = require('@googleforgames/agones');
+const AgonesSDK = require('@google-cloud/agones-sdk');
 const agonesSDK = new AgonesSDK();
 
 const connect = async () => {
