3fb14046e3130ea6a9a5d534bf6b4bf158ddd329
Alexander Apalikov <alexander.apalikov@globant.com>
2020-05-07 17:34:09 +0300

    Update EKS Kubernetes version to 1.15
    Docs and terraform updates.

--- install/terraform/modules/eks/eks.tf
+++ install/terraform/modules/eks/eks.tf
@@ -81,7 +81,7 @@ module "eks" {
   cluster_name    = var.cluster_name
   subnets         = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
-  cluster_version = "1.14"
+  cluster_version = "1.15"
 
   worker_groups_launch_template = [
     {
