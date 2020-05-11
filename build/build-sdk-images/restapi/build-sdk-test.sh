d0056742251d8018c0d7865955f74e4104a8594c
Mark Mandel <markmandel@google.com>
2020-05-11 14:18:41 -0700

    REST SDK Conformance Tests
    Conformance tests for the REST API.
    
    Work on #1507

--- build/build-sdk-images/restapi/build-sdk-test.sh
+++ build/build-sdk-images/restapi/build-sdk-test.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 mkdir /go/src/agones.dev/agones/swagger
 wget -q https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.10/swagger-codegen-cli-2.4.10.jar -O /tmp/swagger-codegen-cli.jar
 java -jar /tmp/swagger-codegen-cli.jar generate -i /go/src/agones.dev/agones/sdks/swagger/sdk.swagger.json  -l go -o /go/src/agones.dev/agones/test/sdk/restapi/swagger
+java -jar /tmp/swagger-codegen-cli.jar generate -i /go/src/agones.dev/agones/sdks/swagger/alpha.swagger.json  -l go -o /go/src/agones.dev/agones/test/sdk/restapi/alpha/swagger
