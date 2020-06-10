googleforgames/agones

commit 58a074692a7e420223dcedbaae6fd545d0f628f0

Fix 404 in CRD generated documentation. (#1621)
- Updated gen-crd-api-reference-docs
- Added our own config to point to the version of K8s we want.
- Manually updated the older CRD ref to point to 1.15, since it doesn't
get updated automatically.
- Added the ability to the htmltest config to disable external testing,
  for easier local dev testing.

Closes #1617

site/gen-api-docs.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -ex
+
 export GOPROXY=http://proxy.golang.org
 echo "using go proxy as a workaround for git.agache.org being down: $GOPROXY"
 
@@ -39,7 +41,7 @@ RESULT="/tmp/agones_crd_api_reference.html"
 # Version to compare
 OLD="/tmp/old_docs.html"
 
-./gen-crd-api-reference-docs --config ./example-config.json --api-dir ../../../agones.dev/agones/pkg/apis/ --out-file $RESULT
+./gen-crd-api-reference-docs --config ../../../agones.dev/agones/site/assets/templates/crd-doc-config.json --api-dir ../../../agones.dev/agones/pkg/apis/ --out-file $RESULT
 awk '/\ feature\ publishVersion/{flag=1;next}/\ \/feature/{flag=0}flag' $FILE > $OLD
 
 # Get the title lines from +++ till empty string
@@ -60,7 +62,7 @@ function sedeasy {
 }
 
 # do we have changes in generated API docs compared to previous version
-diff $RESULT $OLD
+diff $RESULT $OLD || true
 if [ $? -gt 0 ]
 then 
   echo "Output to a file $FILE"

build/build-image/Dockerfile
@@ -89,7 +89,7 @@ RUN curl -sL https://deb.nodesource.com/setup_12.x | bash - && \
 
 # install API reference docs generator
 RUN mkdir -p /go/src/github.com/ahmetb && \
-    cd /go/src/github.com/ahmetb && git clone -b v0.1.1 https://github.com/ahmetb/gen-crd-api-reference-docs && \
+    cd /go/src/github.com/ahmetb && git clone -b v0.2.0 https://github.com/ahmetb/gen-crd-api-reference-docs && \
     cd ./gen-crd-api-reference-docs && go build
 
 # html checker

site/.gitignore
@@ -4,3 +4,4 @@ resources/
 node_modules/
 tech-doc-hugo
 *.json
+!/assets/templates/crd-doc-config.json

site/assets/templates/pkg.tpl
@@ -5,21 +5,23 @@
 <ul>
     {{ range . }}
     <li>
-        <a href="#{{- packageDisplayName . -}}">{{ packageDisplayName . }}</a>
+        <a href="#{{- packageAnchorID . -}}">{{ packageDisplayName . }}</a>
     </li>
     {{ end }}
 </ul>
 {{ end}}
 
 {{ range .packages }}
-    <h2 id="{{- packageDisplayName . -}}">
+    <h2 id="{{- packageAnchorID . -}}">
         {{- packageDisplayName . -}}
     </h2>
 
-    {{ with .DocComments }}
-    <p>
-        {{ safe (renderComments .) }}
-    </p>
+    {{ with (index .GoPackages 0 )}}
+        {{ with .DocComments }}
+        <p>
+            {{ safe (renderComments .) }}
+        </p>
+        {{ end }}
     {{ end }}
 
     Resource Types:
@@ -27,7 +29,7 @@
     {{- range (visibleTypes (sortedTypes .Types)) -}}
         {{ if isExportedType . -}}
         <li>
-            <a href="#{{ typeIdentifier . }}">{{ typeIdentifier . }}</a>
+            <a href="{{ linkForType . }}">{{ typeDisplayName . }}</a>
         </li>
         {{- end }}
     {{- end -}}

site/htmltest.yaml
@@ -16,6 +16,7 @@ TestFilesConcurrently: false
 IgnoreInternalEmptyHash: true
 OutputDir: /go/src/agones.dev/agones/site/tmp
 OutputLogFile: .htmltest.log
+CheckExternal: true
 ExternalTimeout: 60
 IgnoreURLs:
   - http://localhost

commit 65a646e4e5d9780f03771bfdd4f01be3d14bf0e6

Fix replacement bug in gen-api-docs.sh
I introduced a bug in #1621 that means that the publish/expiry
replacement would never happen.

This fixes that issue, while also allowing us to maintain the `set -e`
within the bash script.

site/gen-api-docs.sh
@@ -62,8 +62,7 @@ function sedeasy {
 }
 
 # do we have changes in generated API docs compared to previous version
-diff $RESULT $OLD || true
-if [ $? -gt 0 ]
+if ! diff $RESULT $OLD;
 then 
   echo "Output to a file $FILE"
 