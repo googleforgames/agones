72b3db1a0c92862dd760dd506250c71b8c59e370
Nikhil Athreya <nathreya@google.com>
2020-06-25 21:29:30 +0000

    Removed feature parsing in controller_test.go

--- pkg/gameserverallocations/controller_test.go
+++ pkg/gameserverallocations/controller_test.go
@@ -32,7 +32,6 @@ import (
 	"agones.dev/agones/pkg/gameservers"
 	agtesting "agones.dev/agones/pkg/testing"
 	"agones.dev/agones/pkg/util/apiserver"
-	"agones.dev/agones/pkg/util/runtime"
 	"agones.dev/agones/pkg/util/signals"
 	"github.com/heptiolabs/healthcheck"
 	"github.com/pkg/errors"
@@ -1162,8 +1161,6 @@ func TestMultiClusterAllocationFromRemote(t *testing.T) {
 
 func TestCreateRestClientError(t *testing.T) {
 	t.Parallel()
-	err := runtime.ParseFeatures("")
-	assert.NoError(t, err)
 	t.Run("Missing secret", func(t *testing.T) {
 		c, _ := newFakeController()
 
