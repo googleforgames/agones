f3723b0bfacc9631bb290713cde582095a4e384a
Mark Mandel <markmandel@google.com>
2020-07-06 11:46:32 -0700

    Update Hugo and Docsy (#1671)
    Due to #1668, saw that Hugo can fall over with:
    `fatal error: concurrent map read and map write`
    
    When there are lots of CPUs, so decided it was time to take the
    opportunity to update Hugo.
    
    Only big difference was to enable "unsafe" in the markdown engine, so
    that it would allow HTML in markdown.

--- site/themes/docsy/postcss.config.js
+++ site/themes/docsy/postcss.config.js
@@ -16,17 +16,6 @@ limitations under the License.
 
 module.exports = {
     plugins: {
-        autoprefixer: {
-            browsers: [
-                "Android 2.3",
-                "Android >= 4",
-                "Chrome >= 20",
-                "Firefox >= 24",
-                "Explorer >= 8",
-                "iOS >= 6",
-                "Opera >= 12",
-                "Safari >= 6"
-            ]
-        }
+        autoprefixer: {}
     },
 }
