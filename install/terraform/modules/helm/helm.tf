026be86f3511884c721c50c9a75e9ff351f5c452
Adam Comerford <adam@comerford.net>
2020-07-15 16:18:24 +0100

    Update install/terraform/modules/helm/helm.tf
    Co-authored-by: Alexander Apalikov <alexander.apalikov@globant.com>

--- install/terraform/modules/helm/helm.tf
+++ install/terraform/modules/helm/helm.tf
@@ -154,9 +154,6 @@ resource "helm_release" "agones" {
     value = var.feature_gates
   }
   
-    name  = "agones.featureGates"
-    value = var.feature_gates
-  }
 
   set {
     name  = "gameservers.namespaces"
