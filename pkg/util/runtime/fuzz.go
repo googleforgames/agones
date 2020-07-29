37cbf7155f945c3db7c67f038b34b27d8ff362bd
AdamKorcz <adam@adalogics.com>
2020-07-29 16:26:04 +0100

    1) Added instructions on running the fuzzer locally. 2) Added missing copyright header

--- pkg/util/runtime/fuzz.go
+++ pkg/util/runtime/fuzz.go
@@ -1,6 +1,31 @@
+// Copyright 2020 Google LLC All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package runtime
 
-// Fuzz implements the fuzz test
+// This file holds fuzzers that are implemented
+// using go-fuzz. More info about go-fuzz can
+// be found at https://github.com/dvyukov/go-fuzz
+
+// To run the below fuzzer locally, follow these steps:
+// 1) go get -u github.com/dvyukov/go-fuzz/go-fuzz
+// 2) go get -u github.com/dvyukov/go-fuzz/go-fuzz-build
+// 3) cd to directory of fuzz.go
+// 4) $GOPATH/bin/go-fuzz-build
+// 5) $GOPATH/bin/go-fuzz
+
+// Fuzz implements a fuzzer that targets ParseFeatures
 func Fuzz(data []byte) int {
 	err := ParseFeatures(string(data))
 	if err != nil {
