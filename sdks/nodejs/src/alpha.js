4225416facfe14c6915e69664393818b2b6208fa
Steven Shipton <steven@supersolid.com>
2020-08-04 17:45:38 +0100

    Alpha SDK and example for Node.js (Player tracking) (#1658)
    * update dependencies in sdk and example
    * update build and generate pb
    * add alpha sdk
    * export sdk and alpha sdk
    * cleanup nodejs-simple
    * add nodejs-alpha example
    
    Co-authored-by: Mark Mandel <markmandel@google.com>

---
+++ sdks/nodejs/src/alpha.js
@@ -0,0 +1,130 @@
+// Copyright 2020 Google LLC All Rights Reserved.
+
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const grpc = require('@grpc/grpc-js');
+
+const messages = require('../lib/alpha/alpha_pb');
+const servicesPackageDefinition = require('../lib/alpha/alpha_grpc_pb');
+
+class Alpha {
+	constructor(address, credentials) {
+		const services = grpc.loadPackageDefinition(servicesPackageDefinition);
+		this.client = new services.agones.dev.sdk.alpha.SDK(address, credentials);
+	}
+
+	async playerConnect(playerID) {
+		const request = new messages.PlayerID();
+		request.setPlayerid(playerID);
+
+		return new Promise((resolve, reject) => {
+			this.client.playerConnect(request, (error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response.getBool());
+				}
+			});
+		});
+	}
+
+	async playerDisconnect(playerID) {
+		const request = new messages.PlayerID();
+		request.setPlayerid(playerID);
+
+		return new Promise((resolve, reject) => {
+			this.client.playerDisconnect(request, (error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response.getBool());
+				}
+			});
+		});
+	}
+
+	async setPlayerCapacity(capacity) {
+		const request = new messages.Count();
+		request.setCount(capacity);
+
+		return new Promise((resolve, reject) => {
+			this.client.setPlayerCapacity(request, (error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response.toObject());
+				}
+			});
+		});
+	}
+
+	async getPlayerCapacity() {
+		const request = new messages.Empty();
+
+		return new Promise((resolve, reject) => {
+			this.client.getPlayerCapacity(request, (error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response.getCount());
+				}
+			});
+		});
+	}
+
+	getPlayerCount() {
+		const request = new messages.Empty();
+
+		return new Promise((resolve, reject) => {
+			this.client.getPlayerCount(request, (error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response.getCount());
+				}
+			});
+		});
+	}
+
+	isPlayerConnected(playerID) {
+		const request = new messages.PlayerID();
+		request.setPlayerid(playerID);
+
+		return new Promise((resolve, reject) => {
+			this.client.isPlayerConnected(request, (error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response.getBool());
+				}
+			});
+		});
+	}
+
+	getConnectedPlayers() {
+		const request = new messages.Empty();
+
+		return new Promise((resolve, reject) => {
+			this.client.getConnectedPlayers(request, (error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response.getListList());
+				}
+			});
+		});
+	}
+}
+
+module.exports = Alpha;
--- sdks/nodejs/src/index.js
+++ sdks/nodejs/src/index.js
@@ -12,6 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const AgonesSDK = require('./agonesSDK');
-
-module.exports = AgonesSDK;
+module.exports = require('./agonesSDK');
