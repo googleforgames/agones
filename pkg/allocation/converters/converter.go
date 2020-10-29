1eed5a13adbcafc50b41291fda63ae7395b8a7b1
Pooneh Mortazavi <pooneh@google.com>
2020-10-29 12:29:00 -0700

    Fix kind on GameServerAllocation converter

--- pkg/allocation/converters/converter.go
+++ pkg/allocation/converters/converter.go
@@ -171,10 +171,6 @@ func ConvertAllocationResponseToGSA(in *pb.AllocationResponse) *allocationv1.Gam
 	}
 
 	out := &allocationv1.GameServerAllocation{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       allocationv1.SchemeGroupVersion.Group,
-			APIVersion: allocationv1.SchemeGroupVersion.Version,
-		},
 		Status: allocationv1.GameServerAllocationStatus{
 			State:          allocationv1.GameServerAllocationAllocated,
 			GameServerName: in.GameServerName,
@@ -183,6 +179,7 @@ func ConvertAllocationResponseToGSA(in *pb.AllocationResponse) *allocationv1.Gam
 			Ports:          convertAllocationPortsToGSAAgonesPorts(in.Ports),
 		},
 	}
+	out.SetGroupVersionKind(allocationv1.SchemeGroupVersion.WithKind("GameServerAllocation"))
 
 	return out
 }
