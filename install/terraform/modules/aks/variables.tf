1a4214465a874c0df25613c9326c69bef97028d8
dzmitry.lahoda <dzmitry@lahoda.pro>
2021-05-14 20:53:51 +0300

    imrove aks

--- install/terraform/modules/aks/variables.tf
+++ install/terraform/modules/aks/variables.tf
@@ -33,3 +33,10 @@ variable "client_id" {
 
 variable "client_secret" {
 }
+
+# VMSS is used, so it is unpredictable how NICs will be given to VMs
+# So let Azure to create NICs with Public IPs as gameservers require
+# Azure Managment SDK can be used to obtain these IPs and map Agones GameServers internal IPs to public 
+variable enable_node_public_ip {
+  default = true
+}
