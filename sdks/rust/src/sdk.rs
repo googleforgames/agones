5085d9bc48cf27ac23b8ea69b040a1eef2e7e14e
Jake Shadle <jake.shadle@embark-studios.com>
2021-05-26 09:17:09 +0200

    "Finish" porting to tonic

--- sdks/rust/src/sdk.rs
+++ sdks/rust/src/sdk.rs
@@ -12,349 +12,198 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::env;
-use std::sync::{Arc, Mutex};
-use std::thread::sleep;
-use std::time::Duration;
+use std::{env, time::Duration};
+use tonic::transport::Channel;
 
-use futures::future::TryFutureExt;
-use futures::prelude::*;
-use futures::FutureExt;
-use futures01::future::Future;
+mod api {
+    tonic::include_proto!("agones.dev.sdk");
+}
 
-use grpcio;
-use protobuf::Message;
+use api::sdk_client::SdkClient;
+pub use api::GameServer;
 
-use super::alpha::*;
-use super::errors::*;
-use super::grpc::sdk;
-use super::grpc::sdk_grpc;
-use super::types::*;
+pub type WatchStream = tonic::Streaming<GameServer>;
+
+use crate::{
+    alpha::Alpha,
+    errors::{Error, Result},
+};
+
+#[inline]
+fn empty() -> api::Empty {
+    api::Empty {}
+}
 
 /// SDK is an instance of the Agones SDK
+#[derive(Clone)]
 pub struct Sdk {
-    client: Arc<sdk_grpc::SdkClient>,
-    health: Arc<Mutex<Option<grpcio::ClientCStreamSender<sdk::Empty>>>>,
-    health_receiver: Arc<Mutex<Option<grpcio::ClientCStreamReceiver<sdk::Empty>>>>,
-    alpha: Arc<Alpha>,
+    client: SdkClient<Channel>,
+    health: tokio::sync::mpsc::Sender<api::Empty>,
+    alpha: Alpha,
 }
 
 impl Sdk {
-    /// Starts a new SDK instance, and connects to localhost on port 9357.
-    /// Blocks until connection and handshake are made.
-    /// Times out after ~30 seconds.
-    pub fn new() -> Result<Sdk> {
-        let port = env::var("AGONES_SDK_GRPC_PORT").unwrap_or("9357".to_string());
-        let addr = format!("localhost:{}", port);
-        let env = Arc::new(grpcio::EnvBuilder::new().build());
-        let ch = grpcio::ChannelBuilder::new(env)
-            .keepalive_timeout(Duration::new(30, 0))
-            .connect(&addr);
-        let cli = sdk_grpc::SdkClient::new(ch.clone());
-        let alpha = Alpha::new(ch);
-        let req = sdk::Empty::new();
+    /// Starts a new SDK instance, and connects to localhost on the port specified
+    /// in the `AGONES_SDK_GRPC_PORT` environment variable, or defaults to 9357.
+    pub async fn new(keep_alive: Option<Duration>) -> Result<Self> {
+        // TODO: Add TLS? For some reason the original Cargo.toml was enabling
+        // grpcio's openssl features, but AFAICT the SDK and sidecar only ever
+        // communicate via a non-TLS connection, so seems like we could just
+        // use the simpler client setup code if TLS is absolutely never needed
+        let addr: http::Uri = format!(
+            "http://localhost:{}",
+            env::var("AGONES_SDK_GRPC_PORT")
+                .ok()
+                .and_then(|s| s.parse().ok())
+                .unwrap_or(9357)
+        )
+        .parse()?;
 
-        // Unfortunately there isn't a native way to block until connected
-        // so we had to roll our own.
-        let mut counter = 0;
+        let builder = tonic::transport::channel::Channel::builder(addr)
+            .keep_alive_timeout(keep_alive.unwrap_or_else(|| Duration::from_secs(30)));
+
+        // let mut root_store = rustls::RootCertStore::empty();
+        // root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+        // let mut rusttls_config = rustls::ClientConfig::new();
+        // rusttls_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+        // rusttls_config.root_store = root_store;
+        // let tls_config =
+        //     tonic::transport::ClientTlsConfig::new().rustls_client_config(rusttls_config);
+        // builder = builder.tls_config(tls_config)?;
+
+        let channel = builder.connect().await?;
+        let mut client = SdkClient::new(channel.clone());
+        let alpha = Alpha::new(channel);
+
+        // Loop until we connect. The original implementation just looped once
+        // every second up to a maximum of 30 seconds, but it's better for the
+        // external caller to wrap this in their own timeout, eg
+        // https://docs.rs/tokio/1.6.0/tokio/time/fn.timeout.html
+        let mut connect_interval = tokio::time::interval(Duration::from_millis(100));
+
         loop {
-            counter += 1;
-            match cli.get_game_server(&req) {
-                Ok(_) => break,
-                Err(e) => {
-                    if counter > 30 {
-                        return Err(ErrorKind::Grpc(e).into());
-                    }
-                    sleep(Duration::from_secs(1));
-                    continue;
-                }
+            connect_interval.tick().await;
+
+            if client.get_game_server(empty()).await.is_ok() {
+                break;
             }
         }
 
         // Keep both sender and receiver as RPC is canceled when sender or receiver is dropped
-        let (sender, receiver) = cli.health()?;
-        Ok(Sdk {
-            client: Arc::new(cli),
-            health: Arc::new(Mutex::new(Some(sender))),
-            health_receiver: Arc::new(Mutex::new(Some(receiver))),
-            alpha: Arc::new(alpha),
+        let (sender, mut receiver) = tokio::sync::mpsc::channel(10);
+
+        let health_stream = async_stream::stream! {
+            while let Some(item) = receiver.recv().await {
+                yield item;
+            }
+        };
+
+        client.health(health_stream).await?;
+
+        Ok(Self {
+            client,
+            health: sender,
+            alpha,
         })
     }
 
     /// Alpha returns the Alpha SDK
-    pub fn alpha(&self) -> &Arc<Alpha> {
+    #[inline]
+    pub fn alpha(&self) -> &Alpha {
         &self.alpha
     }
 
     /// Marks the Game Server as ready to receive connections
-    pub fn ready(&self) -> Result<()> {
-        let req = sdk::Empty::default_instance();
-        let res = self.client.ready(req).map(|_| ())?;
-        Ok(res)
+    #[inline]
+    pub async fn ready(&mut self) -> Result<()> {
+        Ok(self.client.ready(empty()).await.map(|_| ())?)
     }
 
     /// Allocate the Game Server
-    pub fn allocate(&self) -> Result<()> {
-        let req = sdk::Empty::default_instance();
-        let res = self.client.allocate(req).map(|_| ())?;
-        Ok(res)
+    #[inline]
+    pub async fn allocate(&mut self) -> Result<()> {
+        Ok(self.client.allocate(empty()).await.map(|_| ())?)
     }
 
     /// Marks the Game Server as ready to shutdown
-    pub fn shutdown(&self) -> Result<()> {
-        let req = sdk::Empty::default_instance();
-        let res = self.client.shutdown(req).map(|_| ())?;
-        Ok(res)
+    #[inline]
+    pub async fn shutdown(&mut self) -> Result<()> {
+        Ok(self.client.shutdown(empty()).await.map(|_| ())?)
     }
 
     /// Sends a ping to the health check to indicate that this server is healthy
-    pub fn health(mut self) -> (Self, Result<()>) {
-        // Avoid `cannot move out of borrowed content` compile error for self.health
-        let h = self.health.lock().unwrap().take();
-        if h.is_none() {
-            return (
-                self,
-                Err(ErrorKind::HealthPingConnectionFailure(
-                    "failed to hold client stream for health ping".to_string(),
-                )
-                .into()),
-            );
-        }
-        let mut h = h.unwrap();
-
-        let req = sdk::Empty::new();
-        let result = h
-            .send((req, grpcio::WriteFlags::default()))
-            .boxed_local()
-            .compat()
-            .wait();
-        match result {
-            Ok(_) => {
-                self.health = Arc::new(Mutex::new(Some(h)));
-                (self, Ok(()))
-            }
-            Err(e) => (self, Err(ErrorKind::Grpc(e).into())),
-        }
-    }
-
-    /// Set a Label value on the backing GameServer record that is stored in Kubernetes
-    pub fn set_label<S>(&self, key: S, value: S) -> Result<()>
-    where
-        S: Into<String>,
-    {
-        let mut kv = sdk::KeyValue::new();
-        kv.set_key(key.into());
-        kv.set_value(value.into());
-        let res = self.client.set_label(&kv).map(|_| ())?;
-        Ok(res)
-    }
-
-    /// Set a Annotation value on the backing Gameserver record that is stored in Kubernetes
-    pub fn set_annotation<S>(&self, key: S, value: S) -> Result<()>
-    where
-        S: Into<String>,
-    {
-        let mut kv = sdk::KeyValue::new();
-        kv.set_key(key.into());
-        kv.set_value(value.into());
-        let res = self.client.set_annotation(&kv).map(|_| ())?;
-        Ok(res)
-    }
-
-    /// Returns most of the backing GameServer configuration and Status
-    pub fn get_gameserver(&self) -> Result<GameServer> {
-        let req = sdk::Empty::new();
-        let res = self
-            .client
-            .get_game_server(&req)
-            .map(|e| GameServer::from_message(e))?;
-        Ok(res)
-    }
-
-    /// Reserve marks the Game Server as Reserved for a given duration, at which point
-    /// it will return the GameServer to a Ready state.
-    /// Do note, the smallest unit available in the time.Duration argument is a second.
-    pub fn reserve(&self, duration: Duration) -> Result<()> {
-        let mut d = sdk::Duration::new();
-        d.set_seconds(duration.as_secs() as i64);
-
-        let res = self.client.reserve(&d).map(|_| ())?;
-        Ok(res)
-    }
-
-    /// Watch the backing GameServer configuration on updated
-    pub fn watch_gameserver<F>(&self, mut watcher: F) -> Result<()>
-    where
-        F: FnMut(GameServer) -> (),
-    {
-        let req = sdk::Empty::new();
-        let mut receiver = self.client.watch_game_server(&req)?;
-        loop {
-            let res = receiver.try_next().boxed_local().compat().wait();
-            match res {
-                Ok(res) => match res {
-                    Some(res) => watcher(GameServer::from_message(res)),
-                    None => break,
-                },
-                Err(e) => {
-                    return Err(e.into());
-                }
-            }
-        }
-        Ok(())
-    }
-
-    /// Starts a new SDK instance, and connects to localhost on port 9357.
-    /// Blocks until connection and handshake are made.
-    /// Times out after ~30 seconds.
-    pub async fn new_async() -> Result<Sdk> {
-        let port = env::var("AGONES_SDK_GRPC_PORT").unwrap_or("9357".to_string());
-        let addr = format!("localhost:{}", port);
-        let env = Arc::new(grpcio::EnvBuilder::new().build());
-        let ch = grpcio::ChannelBuilder::new(env)
-            .keepalive_timeout(Duration::new(30, 0))
-            .connect(&addr);
-        let cli = sdk_grpc::SdkClient::new(ch.clone());
-        let alpha = Alpha::new(ch);
-        let req = sdk::Empty::new();
-
-        // Unfortunately there isn't a native way to block until connected
-        // so we had to roll our own.
-        let mut counter = 0;
-        loop {
-            counter += 1;
-            match cli.get_game_server_async(&req)?.await {
-                Ok(_) => break,
-                Err(e) => {
-                    if counter > 30 {
-                        return Err(ErrorKind::Grpc(e).into());
-                    }
-                    sleep(Duration::from_secs(1));
-                    continue;
-                }
-            }
-        }
-
-        // Keep both sender and receiver as RPC is canceled when sender or receiver is dropped
-        let (sender, receiver) = cli.health()?;
-        Ok(Sdk {
-            client: Arc::new(cli),
-            health: Arc::new(Mutex::new(Some(sender))),
-            health_receiver: Arc::new(Mutex::new(Some(receiver))),
-            alpha: Arc::new(alpha),
+    #[inline]
+    pub async fn health(&self) -> Result<()> {
+        self.health.send(empty()).await.map(|_| ()).map_err(|_| {
+            Error::HealthPingConnectionFailure("tonic receiver was dropped".to_string())
         })
     }
 
-    /// Marks the Game Server as ready to receive connections
-    pub async fn ready_async(&self) -> Result<()> {
-        let req = sdk::Empty::default_instance();
-        let res = self.client.ready_async(req)?.await.map(|_| ())?;
-        Ok(res)
+    /// Set a Label value on the backing GameServer record that is stored in Kubernetes
+    #[inline]
+    pub async fn set_label(
+        &mut self,
+        key: impl Into<String>,
+        value: impl Into<String>,
+    ) -> Result<()> {
+        Ok(self
+            .client
+            .set_label(api::KeyValue {
+                key: key.into(),
+                value: value.into(),
+            })
+            .await
+            .map(|_| ())?)
     }
 
-    /// Allocate the Game Server
-    pub async fn allocate_async(&self) -> Result<()> {
-        let req = sdk::Empty::default_instance();
-        let res = self.client.allocate_async(req)?.await.map(|_| ())?;
-        Ok(res)
-    }
-
-    /// Marks the Game Server as ready to shutdown
-    pub async fn shutdown_async(&self) -> Result<()> {
-        let req = sdk::Empty::default_instance();
-        let res = self.client.shutdown_async(req)?.await.map(|_| ())?;
-        Ok(res)
-    }
-
-    /// Sends a ping to the health check to indicate that this server is healthy
-    pub async fn health_async(&mut self) -> Result<()> {
-        // Avoid `cannot move out of borrowed content` compile error for self.health
-        let h = self.health.lock().unwrap().take();
-        if h.is_none() {
-            return Err(ErrorKind::HealthPingConnectionFailure(
-                "failed to hold client stream for health ping".to_string(),
-            )
-            .into());
-        }
-        let mut h = h.unwrap();
-
-        let req = sdk::Empty::new();
-        match h.send((req, grpcio::WriteFlags::default())).await {
-            Ok(_) => {
-                self.health = Arc::new(Mutex::new(Some(h)));
-                Ok(())
-            }
-            Err(e) => Err(ErrorKind::Grpc(e).into()),
-        }
-    }
-
-    /// Set a Label value on the backing GameServer record that is stored in Kubernetes with the prefix 'agones.dev/sdk-'
-    pub async fn set_label_async<S>(&self, key: S, value: S) -> Result<()>
-    where
-        S: Into<String>,
-    {
-        let mut kv = sdk::KeyValue::new();
-        kv.set_key(key.into());
-        kv.set_value(value.into());
-        let res = self.client.set_label_async(&kv)?.await.map(|_| ())?;
-        Ok(res)
-    }
-
-    /// Set a Annotation value on the backing Gameserver record that is stored in Kubernetes with the prefix 'agones.dev/sdk-'
-    pub async fn set_annotation_async<S>(&self, key: S, value: S) -> Result<()>
-    where
-        S: Into<String>,
-    {
-        let mut kv = sdk::KeyValue::new();
-        kv.set_key(key.into());
-        kv.set_value(value.into());
-        let res = self.client.set_annotation_async(&kv)?.await.map(|_| ())?;
-        Ok(res)
+    /// Set a Annotation value on the backing Gameserver record that is stored in Kubernetes
+    #[inline]
+    pub async fn set_annotation<S>(
+        &mut self,
+        key: impl Into<String>,
+        value: impl Into<String>,
+    ) -> Result<()> {
+        Ok(self
+            .client
+            .set_annotation(api::KeyValue {
+                key: key.into(),
+                value: value.into(),
+            })
+            .await
+            .map(|_| ())?)
     }
 
     /// Returns most of the backing GameServer configuration and Status
-    pub async fn get_gameserver_async(&self) -> Result<GameServer> {
-        let req = sdk::Empty::new();
-        let res = self
+    #[inline]
+    pub async fn get_gameserver(&mut self) -> Result<GameServer> {
+        Ok(self
             .client
-            .get_game_server_async(&req)?
+            .get_game_server(empty())
             .await
-            .map(|e| GameServer::from_message(e))?;
-        Ok(res)
+            .map(|res| res.into_inner())?)
     }
 
     /// Reserve marks the Game Server as Reserved for a given duration, at which point
     /// it will return the GameServer to a Ready state.
-    /// Do note, the smallest unit available in the time.Duration argument is a second.
-    pub async fn reserve_async(&self, duration: Duration) -> Result<()> {
-        let mut d = sdk::Duration::new();
-        d.set_seconds(duration.as_secs() as i64);
-
-        let res = self.client.reserve_async(&d)?.await.map(|_| ())?;
-        Ok(res)
+    /// Do note, the smallest unit available in the time.Duration argument is one second.
+    #[inline]
+    pub async fn reserve(&mut self, duration: Duration) -> Result<()> {
+        Ok(self
+            .client
+            .reserve(api::Duration {
+                seconds: std::cmp::max(duration.as_secs() as i64, 1),
+            })
+            .await
+            .map(|_| ())?)
     }
 
     /// Watch the backing GameServer configuration on updated
-    pub async fn watch_gameserver_async<F>(&self, mut watcher: F) -> Result<()>
-    where
-        F: FnMut(GameServer) -> (),
-    {
-        let req = sdk::Empty::new();
-        let mut receiver = self.client.watch_game_server(&req)?;
-        while let Some(e) = receiver.try_next().await? {
-            watcher(GameServer::from_message(e));
-        }
-        Ok(())
-    }
-}
-
-impl Clone for Sdk {
-    fn clone(&self) -> Self {
-        Self {
-            client: Arc::clone(&self.client),
-            health: self.health.clone(),
-            health_receiver: self.health_receiver.clone(),
-            alpha: Arc::clone(&self.alpha),
-        }
+    #[inline]
+    pub async fn watch_gameserver<F>(&mut self) -> Result<WatchStream> {
+        Ok(self
+            .client
+            .watch_game_server(empty())
+            .await
+            .map(|stream| stream.into_inner())?)
     }
 }
