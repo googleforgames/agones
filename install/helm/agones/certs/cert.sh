3756583601cf89ea6d344471cff865f9ea9eaaef
cindy52 <ciguo@google.com>
2021-07-01 20:36:58 -0700

    Upgrade/go 1.15 (#2167)
    * upgrade go to 1.15 and add SAN to all certs

--- install/helm/agones/certs/cert.sh
+++ install/helm/agones/certs/cert.sh
@@ -4,7 +4,7 @@ echo "Generating certs..."
 echo "Email should be: emailAddress=agones-discuss@googlegroups.com"
 echo "Common Name should be: agones-controller-service.agones-system.svc"
 openssl genrsa -out server.key 2048
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
+openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650 -addext 'subjectAltName=DNS:agones-controller-service.agones-system.svc'
 
 echo "caBundle:"
 base64 -w 0 server.crt
