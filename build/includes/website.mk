05ae5d47ef567507c12aab1dc4cc1d54a7509480
Mark Mandel <markmandel@google.com>
2021-09-01 16:17:43 -0700

    Docs: Integration Pattern - Reusing GameServers
    Full title: Reusing Allocated GameServers for more than one game session
    
    Describing how you can return a `GameServer` to `Ready` through the SDK
    at the end of a game session, depending on the needs of your game.

--- build/includes/website.mk
+++ build/includes/website.mk
@@ -79,6 +79,7 @@ site-images: $(site_path)/static/diagrams/gameserver-lifecycle.puml.png
 site-images: $(site_path)/static/diagrams/gameserver-reserved.puml.png
 site-images: $(site_path)/static/diagrams/canary-testing.puml.png
 site-images: $(site_path)/static/diagrams/allocation-player-capacity.puml.png
+site-images: $(site_path)/static/diagrams/reusing-gameservers.puml.png
 
 # generate pngs from dot files
 %.dot.png: %.dot
