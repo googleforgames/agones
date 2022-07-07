7a59409a6fca54912f60adb43639fd42a5231c91
Mark Mandel <markmandel@google.com>
2022-07-06 21:29:48 -0700

    Fix minikube dev tooling (#2662)
    The update to multi-arch images broke the minikube dev tooling, this
    fixes that.
    
    Only caveat being, that I can only test on amd64 architecture. In
    theory, it might work on a M1, but if not, we can switch our which
    images are loaded before being re-tagged based on host architecture.

--- build/includes/minikube.mk
+++ build/includes/minikube.mk
@@ -28,7 +28,7 @@ minikube_cert_mount := ~/.minikube:$(HOME)/.minikube
 # of the right version.
 minikube-test-cluster: DOCKER_RUN_ARGS+=--network=host -v $(minikube_cert_mount)
 minikube-test-cluster: $(ensure-build-image)
-	$(MINIKUBE) start --kubernetes-version v1.22.9 -p $(MINIKUBE_PROFILE) --vm-driver $(MINIKUBE_DRIVER)
+	$(MINIKUBE) start --kubernetes-version v1.22.9 -p $(MINIKUBE_PROFILE) --driver $(MINIKUBE_DRIVER)
 
 # Connecting to minikube requires so enhanced permissions, so use this target
 # instead of `make shell` to start an interactive shell for development on minikube.
@@ -39,9 +39,14 @@ minikube-shell: $(ensure-build-image)
 # via `make build` or `make build-images` into the "agones" minikube instance.
 minikube-push:
 	$(MINIKUBE) image load $(sidecar_linux_amd64_tag) -p $(MINIKUBE_PROFILE)
-	$(MINIKUBE) image load $(controller_tag) -p $(MINIKUBE_PROFILE)
-	$(MINIKUBE) image load $(ping_tag) -p $(MINIKUBE_PROFILE)
-	$(MINIKUBE) image load $(allocator_tag) -p $(MINIKUBE_PROFILE)
+	$(MINIKUBE) image load $(controller_amd64_tag) -p $(MINIKUBE_PROFILE)
+	$(MINIKUBE) image load $(ping_amd64_tag) -p $(MINIKUBE_PROFILE)
+	$(MINIKUBE) image load $(allocator_amd64_tag) -p $(MINIKUBE_PROFILE)
+
+	$(MINIKUBE) image tag $(sidecar_linux_amd64_tag) $(sidecar_tag) -p $(MINIKUBE_PROFILE)
+	$(MINIKUBE) image tag $(controller_amd64_tag) $(controller_tag) -p $(MINIKUBE_PROFILE)
+	$(MINIKUBE) image tag $(ping_amd64_tag) $(ping_tag) -p $(MINIKUBE_PROFILE)
+	$(MINIKUBE) image tag $(allocator_amd64_tag) $(allocator_tag) -p $(MINIKUBE_PROFILE)
 
 # Installs the current development version of Agones into the Kubernetes cluster.
 # Use this instead of `make install`, as it disables PullAlways on the install.yaml
