73cdabc4bf0f238ce2d913df8352d11b3a888445
Max Hayman <maxhayman@maxhayman.co.uk>
2022-07-12 15:31:41 +0100

    Support for Unity Alpha SDK (#2600)
    * Support for Unity Alpha SDK
    
    * Documentation for Alpha SDK features of Unity
    
    * Add feature gates for Unity Alpha SDK documentation
    
    * Update site/content/en/docs/Guides/Client SDKs/unity.md
    
    Co-authored-by: Mark Mandel <markmandel@google.com>
    
    * Update site/content/en/docs/Guides/Client SDKs/unity.md
    
    Co-authored-by: Mark Mandel <markmandel@google.com>
    
    * Fixes for expiry and alpha shortcode
    
    Had to wrangle the formatting of the alpha shortcode to
    handle shortcode in shortcode rendering, and also updated everything to 1.25.0.
    
    Co-authored-by: Mark Mandel <markmandel@google.com>

---
+++ sdks/unity/AgonesAlphaSdk.cs
@@ -0,0 +1,191 @@
+﻿// Copyright 2022 Google LLC
+// All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Agones.Model;
+using MiniJSON;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Agones
+{
+    /// <summary>
+    /// Agones Alpha SDK for Unity.
+    /// </summary>
+    public class AgonesAlphaSdk : AgonesSdk
+    {
+        #region AgonesRestClient Public Methods
+
+        private struct Player
+        {
+            public string playerID;
+
+            public Player(string playerId)
+            {
+                this.playerID = playerId;
+            }
+        }
+
+        /// <summary>
+        /// This function increases the SDK’s stored player count by one, and appends this playerID to GameServer.Status.Players.IDs.
+        /// Returns true and adds the playerID to the list of playerIDs if the playerIDs was not already in the list of connected playerIDs.
+        /// </summary>
+        /// <returns>True if the playerID was added to the list of playerIDs</returns>
+        public async Task<bool> PlayerConnect(string id)
+        {
+            string json = JsonUtility.ToJson(new Player(playerId: id));
+            return await SendRequestAsync("/alpha/player/connect", json).ContinueWith(task => task.Result.ok);
+        }
+
+        /// <summary>
+        /// This function decreases the SDK’s stored player count by one, and removes the playerID from GameServer.Status.Players.IDs.
+        /// Will return true and remove the supplied playerID from the list of connected playerIDs if the playerID value exists within the list.
+        /// </summary>
+        /// <returns>True if the playerID was removed from the list of playerIDs</returns>
+        public async Task<bool> PlayerDisconnect(string id)
+        {
+            string json = JsonUtility.ToJson(new Player(playerId: id));
+            return await SendRequestAsync("/alpha/player/disconnect", json).ContinueWith(task => task.Result.ok);
+        }
+
+        private struct Capacity
+        {
+            public long count;
+
+            public Capacity(long count)
+            {
+                this.count = count;
+            }
+        }
+
+        /// <summary>
+        /// This changes the player capacity to a new value.
+        /// </summary>
+        /// <returns>gRPC Status of the request</returns>
+        public async Task<bool> SetPlayerCapacity(long count)
+        {
+            string json = JsonUtility.ToJson(new Capacity(count: count));
+            return await SendRequestAsync("/alpha/player/capacity", json, UnityWebRequest.kHttpVerbPUT).ContinueWith(task => task.Result.ok);
+        }
+        
+
+        /// <summary>
+        /// This returns the last player capacity that was set through the SDK.
+        /// If the player capacity is set from outside the SDK, use SDK.GameServer() instead.
+        /// </summary>
+        /// <returns>Player capacity</returns>
+        public async Task<long> GetPlayerCapacity()
+        {
+            var result =  await SendRequestAsync("/alpha/player/capacity", "{}", UnityWebRequest.kHttpVerbGET);
+            
+            if (!result.ok)
+            {
+                return 0;
+            }
+
+            if (Json.Deserialize(result.json) is not Dictionary<string, object> data
+                || !data.TryGetValue("count", out object countObject)
+                || countObject is not string countString
+                || !long.TryParse(countString, out long count))
+            {
+                return 0;
+            }
+            
+            return count;
+        }
+
+        /// <summary>
+        /// Returns the current player count.
+        /// </summary>
+        /// <returns>Player count</returns>
+        public async Task<long> GetPlayerCount()
+        {
+            var result =  await SendRequestAsync("/alpha/player/count", "{}", UnityWebRequest.kHttpVerbGET);
+            
+            if (!result.ok)
+            {
+                return 0;
+            }
+
+            if (Json.Deserialize(result.json) is not Dictionary<string, object> data
+                || !data.TryGetValue("count", out object countObject)
+                || countObject is not string countString
+                || !long.TryParse(countString, out long count))
+            {
+                return 0;
+            }
+            
+            return count;
+        }
+
+        /// <summary>
+        /// This returns if the playerID is currently connected to the GameServer.
+        /// This is always accurate, even if the value hasn’t been updated to the GameServer status yet.
+        /// </summary>
+        /// <returns>True if the playerID is currently connected</returns>
+        public async Task<bool> IsPlayerConnected(string id)
+        {
+            var result = await SendRequestAsync($"/alpha/player/connected/{id}", "{}", UnityWebRequest.kHttpVerbGET);
+            
+            if (!result.ok)
+            {
+                return false;
+            }
+
+            if (Json.Deserialize(result.json) is not Dictionary<string, object> data
+                || !data.TryGetValue("bool", out object boolObject)
+                || boolObject is not bool resultBool)
+            {
+                return false;
+            }
+            
+            return resultBool;
+        }
+
+        /// <summary>
+        /// This returns the list of the currently connected player ids.
+        /// This is always accurate, even if the value has not been updated to the Game Server status yet.
+        /// </summary>
+        /// <returns>The list of the currently connected player ids</returns>
+        public async Task<List<string>> GetConnectedPlayers()
+        {
+            var result =  await SendRequestAsync("/alpha/player/connected", "{}", UnityWebRequest.kHttpVerbGET);
+            
+            if (!result.ok)
+            {
+                return new List<string>();
+            }
+
+            if (Json.Deserialize(result.json) is not Dictionary<string, object> data
+                || !data.TryGetValue("list", out object listObject)
+                || listObject is not List<object> list)
+            {
+                return new List<string>();
+            }
+            
+            return list.Where(l => l is string).Select(l => l.ToString()).ToList();;
+        }
+        
+        #endregion
+
+    }
+}
--- site/content/en/docs/Guides/Client SDKs/unity.md
+++ site/content/en/docs/Guides/Client SDKs/unity.md
@@ -10,6 +10,7 @@ Check the [Client SDK Documentation]({{< relref "_index.md" >}}) for more detail
 
 ## SDK Functionality
 
+{{% feature expiryVersion="1.25.0" %}}
 | Area                 | Action                   | Implemented                   |
 |----------------------|--------------------------|-------------------------------|
 | Lifecycle            | Ready                    | ✔️                            |
@@ -28,6 +29,27 @@ Check the [Client SDK Documentation]({{< relref "_index.md" >}}) for more detail
 | Player Tracking      | PlayerConnect            | ❌️                            | 
 | Player Tracking      | PlayerDisconnect         | ❌️                            | 
 | Player Tracking      | SetPlayerCapacity        | ❌️                            | 
+{{% /feature %}}
+{{% feature publishVersion="1.25.0" %}}
+| Area                 | Action                   | Implemented                   |
+|----------------------|--------------------------|-------------------------------|
+| Lifecycle            | Ready                    | ✔️                            |
+| Lifecycle            | Health                   | ✔️                            | 
+| Lifecycle            | Reserve                  | ✔️                            | 
+| Lifecycle            | Allocate                 | ✔️                            | 
+| Lifecycle            | Shutdown                 | ✔️                            | 
+| Configuration        | GameServer               | ✔️                            | 
+| Configuration        | Watch                    | ✔️                            | 
+| Metadata             | SetAnnotation            | ✔️                            | 
+| Metadata             | SetLabel                 | ✔️                            | 
+| Player Tracking      | GetConnectedPlayers      | ✔️                            | 
+| Player Tracking      | GetPlayerCapacity        | ✔️                            | 
+| Player Tracking      | GetPlayerCount           | ✔️                            | 
+| Player Tracking      | IsPlayerConnected        | ✔️                            | 
+| Player Tracking      | PlayerConnect            | ✔️                            | 
+| Player Tracking      | PlayerDisconnect         | ✔️                            | 
+| Player Tracking      | SetPlayerCapacity        | ✔️                            | 
+{{% /feature %}}
 
 Additional methods have been added for ease of use:
 
@@ -118,6 +140,83 @@ configuration changes.
 ```csharp
 agones.WatchGameServer(gameServer => Debug.Log($"Server - Watch {gameServer}"));
 ```
+
+{{% feature publishVersion="1.25.0" %}}
+
+## Player Tracking
+
+{{< alpha title="Player Tracking" gate="PlayerTracking" >}}
+
+To use alpha features use the AgonesAlphaSDK class.
+
+```csharp
+var agones = agonesGameObject.GetComponent<Agones.AgonesAlphaSdk>();
+```
+
+### Alpha: PlayerConnect
+
+This method increases the SDK’s stored player count by one, and appends this playerID to GameServer.Status.Players.IDs.
+Returns true and adds the playerID to the list of playerIDs if the playerIDs was not already in the list of connected playerIDs.
+
+```csharp
+bool ok = await agones.PlayerConnect(playerId);
+```
+
+### Alpha: PlayerDisconnect
+
+This function decreases the SDK’s stored player count by one, and removes the playerID from GameServer.Status.Players.IDs.
+Will return true and remove the supplied playerID from the list of connected playerIDs if the playerID value exists within the list.
+
+```csharp
+bool ok = await agones.PlayerDisconnect(playerId);
+```
+
+### Alpha: SetPlayerCapacity
+
+Update the `GameServer.Status.Players.Capacity` value with a new capacity.
+
+```csharp
+var capacity = 100;
+bool ok = await agones.SetPlayerCapacity(capacity);
+```
+
+### Alpha: GetPlayerCapacity
+
+This function retrieves the current player capacity `GameServer.Status.Players.Capacity`. 
+This is always accurate from what has been set through this SDK, even if the value has yet to be updated on the GameServer status resource.
+
+```csharp
+long capacity = await agones.GetPlayerCapacity();
+```
+
+### Alpha: GetPlayerCount
+
+Returns the current player count
+
+```csharp
+long count = await agones.GetPlayerCount();
+```
+
+### Alpha: IsPlayerConnected
+
+This returns if the playerID is currently connected to the GameServer.
+This is always accurate, even if the value hasn’t been updated to the GameServer status yet.
+
+```csharp
+bool isConnected = await agones.IsPlayerConnected(playerId);
+```
+
+
+### Alpha: GetConnectedPlayers
+
+This returns a list of the playerIDs that are currently connected to the GameServer.
+
+```csharp
+List<string> players = await agones.GetConnectedPlayers();
+```
+
+{{% /feature %}}
+
 
 {{% alert title="Warning" color="warning"%}}
 The following code causes deadlock. Do not use a `Wait` method with the returned Task.
