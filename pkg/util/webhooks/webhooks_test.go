c1b863e7af12c3248b194faf522d715c96759081
Mengye (Max) Gong <8364575+gongmax@users.noreply.github.com>
2022-09-26 10:37:16 -0700

    Bump the example images version (#2751)

--- pkg/util/webhooks/webhooks_test.go
+++ pkg/util/webhooks/webhooks_test.go
@@ -163,7 +163,7 @@ func TestWebHookFleetValidationHandler(t *testing.T) {
 							"template": {
 								"spec": {
 									"containers": [{
-										"image": "gcr.io/agones-images/simple-game-server:0.13",
+										"image": "gcr.io/agones-images/simple-game-server:0.14",
 										"name": false
 									}]
 								}
