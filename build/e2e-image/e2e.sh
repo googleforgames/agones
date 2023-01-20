e537119b30d802c21db2904463aa86d836f69e09
Zach Loafman <zml@google.com>
2023-01-20 00:07:59 +0000

    Add GKE Autopilot to e2e
    Run e2e tests against Autopilot 1.24 cluster. Mark as `allowFailure`
    for now just to get a sense for whether it works - I will manually
    scan CI after this goes in.
    
    We probably should script additions/mutations to this in the
    future...
    
    Along the way: Skip the TestGameServerEvicted on Autopilot, filed
    it turns out I edited my merge branch instead. :(

--- build/e2e-image/e2e.sh
+++ build/e2e-image/e2e.sh
@@ -15,13 +15,15 @@
 # limitations under the License.
 
 FEATURES=$1
-REGISTRY=$2
+CLOUD_PRODUCT=$2
+REGISTRY=$3
 
 echo $FEATURES
 echo $REGISTRY
 set -e
 echo "installing current release"
-DOCKER_RUN= make install FEATURE_GATES='"'$FEATURES'"' REGISTRY='"'$REGISTRY'"'
+# TODO(#2797): Remove when `SplitControllerAndExtensions` => safe-to-evict=true
+DOCKER_RUN= make install FEATURE_GATES='"'$FEATURES'"' REGISTRY='"'$REGISTRY'"' CONTROLLER_SAFE_TO_EVICT=true
 echo "starting e2e test"
-DOCKER_RUN= make test-e2e ARGS=-parallel=16 E2E_USE_GOTESTSUM=true GOTESTSUM_VERBOSE=true FEATURE_GATES='"'$FEATURES'"'
+DOCKER_RUN= make test-e2e ARGS=-parallel=16 E2E_USE_GOTESTSUM=true GOTESTSUM_VERBOSE=true FEATURE_GATES='"'$FEATURES'"' CLOUD_PRODUCT='"'$CLOUD_PRODUCT'"'
 echo "completed e2e test"
