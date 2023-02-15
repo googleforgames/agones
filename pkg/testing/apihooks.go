8298a72453ba65c10e452e7b8d79c586891548db
Mark Mandel <markmandel@google.com>
2023-02-15 13:34:21 -0800

    Fix for `make gen-crd-client`
    Having `agonesv1.FakeApiHooks` be public means that the code generation
    toolkit will see it as a generation target, and then get all confused.
    
    Fix is to make it private, and keep it's stubbable capabilities.
    
    Since we also need a mock for other parts of the Agones system,
    I also created a no-op version for general usage as well in the
    `testing` package. (We can't use this one in `agonesvs`, since Go
    will complain about cyclic dependencies).

---
+++ pkg/testing/apihooks.go
@@ -0,0 +1,48 @@
+// Copyright 2023 Google LLC All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testing
+
+import (
+	"agones.dev/agones/pkg/apis"
+	agonesv1 "agones.dev/agones/pkg/apis/agones/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// FakeAPIHooks is a no-op, fake implementation of APIHooks
+type FakeAPIHooks struct {
+}
+
+var _ agonesv1.APIHooks = FakeAPIHooks{}
+
+// ValidateGameServerSpec is called by GameServer.Validate to allow for product specific validation.
+func (f FakeAPIHooks) ValidateGameServerSpec(_ *agonesv1.GameServerSpec) []metav1.StatusCause {
+	return nil
+}
+
+// ValidateScheduling is called by Fleet and GameServerSet Validate() to allow for product specific validation of scheduling strategy.
+func (f FakeAPIHooks) ValidateScheduling(_ apis.SchedulingStrategy) []metav1.StatusCause {
+	return nil
+}
+
+// MutateGameServerPodSpec is called by createGameServerPod to allow for product specific pod mutation.
+func (f FakeAPIHooks) MutateGameServerPodSpec(_ *agonesv1.GameServerSpec, podSpec *corev1.PodSpec) error {
+	return nil
+}
+
+// SetEviction is called by gs.Pod to enforce GameServer.Status.Eviction.
+func (f FakeAPIHooks) SetEviction(_ agonesv1.EvictionSafe, pod *corev1.Pod) error {
+	return nil
+}
