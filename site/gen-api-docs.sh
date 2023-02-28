d9f8c6a060c1a9f1cee21281425ee6dc6ad65197
Kalaiselvi Murugesan <kalaiselvim@google.com>
2023-02-28 21:24:24 +0000

    Release 1.30.0 - GKE Autopilot and Multiple Kubernetes version support

--- site/gen-api-docs.sh
+++ site/gen-api-docs.sh
@@ -58,7 +58,6 @@ printf "\n" >> $TITLE
 doc_version=$(grep 'feature publishVersion=' $FILE )
 echo $doc_version
 publish='{{% feature publishVersion="'$VERSION'" %}}'
-expiry='{{% feature expiryVersion="'$VERSION'" %}}'
 
 #Get previous expiry version tag
 sed '/\ expiryVersion="'$VERSION'"/,/%\ \/feature/!d;/%\ \/feature/q' $FILE > $EXPIRY_DOC
