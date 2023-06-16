a0903b4805b227b1f8a61f132139be11dd4fcc05
Kalaiselvim <117940852+Kalaiselvi84@users.noreply.github.com>
2023-06-16 16:11:37 -0700

    Remove Feature Expiry Version Shortcodes (#3210)
    * Remove feature expiry version shortcodes
    
    * resolve linting issues
    
    * resolve linting issues
    
    * Remove feature publish version block without content
    
    * fine-tuning

--- build/includes/website.mk
+++ build/includes/website.mk
@@ -110,6 +110,11 @@ test-gen-api-docs: ensure-build-image
 	sort $(expected_docs) > /tmp/result.sorted
 	diff -bB /tmp/result.sorted /tmp/generated.html.sorted
 
+# Remove feature expiry/publish version shortcodes update in site/content/en/docs
+feature-shortcode-update: ensure-build-image
+	docker run --rm $(common_mounts) --workdir=$(mount_path) $(DOCKER_RUN_ARGS) $(build_tag) \
+		go run build/scripts/feature-shortcode-update/main.go -version=$(version)
+
 # update SDKS/Install version
 sdk-update-version: ensure-build-image
 	docker run --rm $(common_mounts) --workdir=$(mount_path) $(DOCKER_RUN_ARGS) $(build_tag) \
