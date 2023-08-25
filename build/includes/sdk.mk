423ec211bf5a11d55b0e2bbfb63c0b28e0ee8509
Zach Loafman <zml@google.com>
2023-08-25 16:49:35 +0000

    Verify gen-all-sdk-grpc has run
    Adds a build step to verify that `make gen-all-sdk-grpc` has run.
    
    Credit to @Kalaiselvi84 for original #3310
    
    Sample output:
    ```
    === Diffing workspace after 'make gen-all-sdk-grpc'
    *** Failure: Differences found:
    D    sdks/rust/proto/allocation/allocation.proto
    D    sdks/rust/proto/googleapis/google/api/annotations.proto
    D    sdks/rust/proto/googleapis/google/api/client.proto
    D    sdks/rust/proto/googleapis/google/api/field_behavior.proto
    D    sdks/rust/proto/googleapis/google/api/http.proto
    D    sdks/rust/proto/googleapis/google/api/launch_stage.proto
    D    sdks/rust/proto/googleapis/google/api/resource.proto
    D    sdks/rust/proto/grpc-gateway/LICENSE.txt
    D    sdks/rust/proto/grpc-gateway/protoc-gen-openapiv2/options/annotations.proto
    D    sdks/rust/proto/grpc-gateway/protoc-gen-openapiv2/options/openapiv2.proto
    D    sdks/rust/proto/sdk/alpha/alpha.proto
    D    sdks/rust/proto/sdk/beta/beta.proto
    D    sdks/rust/proto/sdk/sdk.proto
    ```
    
    (Note this sample output was also what happens when I just run this
    check on my desktop because the `rust` generation spews files as root.)

--- build/includes/sdk.mk
+++ build/includes/sdk.mk
@@ -216,3 +216,19 @@ sdk-shell-csharp:
 sdk-publish-csharp: RELEASE_VERSION ?= $(base_version)
 sdk-publish-csharp:
 	$(MAKE) run-sdk-command-csharp COMMAND=publish VERSION=$(RELEASE_VERSION) DOCKER_RUN_ARGS="$(DOCKER_RUN_ARGS) -it"
+
+# difference in sdks before and after gen-all-sdk-grpc target
+test-gen-all-sdk-grpc:
+	make gen-all-sdk-grpc
+	@echo; \
+	echo "=== Diffing workspace after 'make gen-all-sdk-grpc'"; \
+	diff_output=$$(git diff --name-status HEAD -- ../sdks); \
+	diff_output_test_sdk=$$(git diff --name-status HEAD -- ../test/sdk); \
+	if [ -z "$$diff_output" ] && [ -z "$$diff_output_test_sdk" ]; then \
+		echo "+++ Success: No differences found."; \
+	else \
+		echo "*** Failure: Differences found:"; \
+		echo "$$diff_output"; \
+		echo "$$diff_output_test_sdk"; \
+		exit 1; \
+	fi
