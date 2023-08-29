d625a4e9371c6055d068d5e7495d7e486d6eca9f
Kalaiselvi Murugesan <kalaiselvim@google.com>
2023-08-29 16:19:45 +0000

    modified sh file

--- build/install-release.sh
+++ build/install-release.sh
@@ -16,14 +16,27 @@
 
 echo "Listing Helm releases in agones-system namespace..."
 helm ls -n agones-system
+
 echo "Removing Agones deployment from agones-system namespace..."
 helm uninstall -n agones-system agones || echo "Failed to uninstall. Consider deleting the current cluster and setting up a new one. Refer to https://agones.dev/site/docs/installation/creating-cluster/gke/#create-a-standard-mode-cluster-for-agones"
+
+echo "Listing pods in the agones-system namespace..."
+kubectl get pods -n agones-system
+
+echo "Deleting agones-system namespace..."
+kubectl delete ns agones-system
+echo "Agones system namespace deleted."
+
 echo "Helm repo update to fetch the latest version of Agones..."
 helm repo update
+
 echo "Verifying the new version..."
 helm search repo agones --versions --devel
+
 echo "Installing Agones in agones-system namespace..."
 helm install --create-namespace --namespace=agones-system agones agones/agones
+
 echo "Listing all pods in agones-system namespace..."
 kubectl get pods --namespace agones-system
+
 echo "Execute any of the test functions from test/e2e directory to complete the smoke test."
