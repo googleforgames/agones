85637681a8400e5cb80dac8009508df61c9ffacf
Ivy Gooch <igooch@google.com>
2023-09-11 21:57:33 +0000

    Adds Go SDK client Counter functions

--- sdks/go/alpha_test.go
+++ sdks/go/alpha_test.go
@@ -72,11 +72,154 @@ func TestAlphaGetAndSetPlayerCapacity(t *testing.T) {
 	assert.Equal(t, []string{playerID}, list)
 }
 
+func TestAlphaGetAndUpdateCounter(t *testing.T) {
+	mock := &alphaMock{}
+	// Counters must be predefined in the GameServer resource on creation.
+	mock.counters = make(map[string]*alpha.Counter)
+	sessions := alpha.Counter{
+		Name:     "sessions",
+		Count:    21,
+		Capacity: 42,
+	}
+	games := alpha.Counter{
+		Name:     "games",
+		Count:    12,
+		Capacity: 24,
+	}
+	gamers := alpha.Counter{
+		Name:     "gamers",
+		Count:    263,
+		Capacity: 500,
+	}
+	mock.counters["sessions"] = &alpha.Counter{
+		Name:     "sessions",
+		Count:    21,
+		Capacity: 42,
+	}
+	mock.counters["games"] = &alpha.Counter{
+		Name:     "games",
+		Count:    12,
+		Capacity: 24,
+	}
+	mock.counters["gamers"] = &alpha.Counter{
+		Name:     "gamers",
+		Count:    263,
+		Capacity: 500,
+	}
+	a := Alpha{
+		client: mock,
+	}
+
+	t.Parallel()
+
+	t.Run("Set Counter and Set Capacity", func(t *testing.T) {
+		counter, err := a.GetCounter("sessions")
+		assert.NoError(t, err)
+		assert.Equal(t, &sessions, &counter)
+
+		wantCapacity := int64(25)
+		ok, err := a.SetCounterCapacity("sessions", wantCapacity)
+		assert.NoError(t, err)
+		assert.True(t, ok)
+
+		capacity, err := a.GetCounterCapacity("sessions")
+		assert.NoError(t, err)
+		assert.Equal(t, wantCapacity, capacity)
+
+		wantCount := int64(10)
+		ok, err = a.SetCounterCount("sessions", wantCount)
+		assert.NoError(t, err)
+		assert.True(t, ok)
+
+		count, err := a.GetCounterCount("sessions")
+		assert.NoError(t, err)
+		assert.Equal(t, wantCount, count)
+	})
+
+	t.Run("Set Counter and Set Capacity Non Defined Counter", func(t *testing.T) {
+		counter, err := a.GetCounter("secessions")
+		assert.Error(t, err)
+		assert.Empty(t, &counter)
+
+		ok, err := a.SetCounterCapacity("secessions", int64(100))
+		assert.Error(t, err)
+		assert.False(t, ok)
+
+		ok, err = a.SetCounterCount("secessions", int64(0))
+		assert.Error(t, err)
+		assert.False(t, ok)
+	})
+
+	// nolint:dupl // testing DecrementCounter and IncrementCounter are not duplicates.
+	t.Run("Decrement Counter Fails then Success", func(t *testing.T) {
+		counter, err := a.GetCounter("games")
+		assert.NoError(t, err)
+		assert.Equal(t, &games, &counter)
+
+		ok, err := a.DecrementCounter("games", 21)
+		assert.Error(t, err)
+		assert.False(t, ok)
+
+		count, err := a.GetCounterCount("games")
+		assert.NoError(t, err)
+		assert.Equal(t, games.Count, count)
+
+		ok, err = a.DecrementCounter("games", -12)
+		assert.Error(t, err)
+		assert.False(t, ok)
+
+		count, err = a.GetCounterCount("games")
+		assert.NoError(t, err)
+		assert.Equal(t, games.Count, count)
+
+		ok, err = a.DecrementCounter("games", 12)
+		assert.NoError(t, err)
+		assert.True(t, ok)
+
+		count, err = a.GetCounterCount("games")
+		assert.NoError(t, err)
+		assert.Equal(t, int64(0), count)
+	})
+
+	// nolint:dupl // testing DecrementCounter and IncrementCounter are not duplicates.
+	t.Run("Increment Counter Fails then Success", func(t *testing.T) {
+		counter, err := a.GetCounter("gamers")
+		assert.NoError(t, err)
+		assert.Equal(t, &gamers, &counter)
+
+		ok, err := a.IncrementCounter("gamers", 250)
+		assert.Error(t, err)
+		assert.False(t, ok)
+
+		count, err := a.GetCounterCount("gamers")
+		assert.NoError(t, err)
+		assert.Equal(t, gamers.Count, count)
+
+		ok, err = a.IncrementCounter("gamers", -237)
+		assert.Error(t, err)
+		assert.False(t, ok)
+
+		count, err = a.GetCounterCount("gamers")
+		assert.NoError(t, err)
+		assert.Equal(t, gamers.Count, count)
+
+		ok, err = a.IncrementCounter("gamers", 237)
+		assert.NoError(t, err)
+		assert.True(t, ok)
+
+		count, err = a.GetCounterCount("gamers")
+		assert.NoError(t, err)
+		assert.Equal(t, int64(500), count)
+	})
+
+}
+
 type alphaMock struct {
 	capacity           int64
 	playerCount        int64
 	playerConnected    string
 	playerDisconnected string
+	counters           map[string]*alpha.Counter
 }
 
 func (a *alphaMock) PlayerConnect(ctx context.Context, id *alpha.PlayerID, opts ...grpc.CallOption) (*alpha.Bool, error) {
@@ -112,16 +255,45 @@ func (a *alphaMock) GetPlayerCount(ctx context.Context, in *alpha.Empty, opts ..
 	return &alpha.Count{Count: a.playerCount}, nil
 }
 
-// GetCounter to be implemented
 func (a *alphaMock) GetCounter(ctx context.Context, in *alpha.GetCounterRequest, opts ...grpc.CallOption) (*alpha.Counter, error) {
-	// TODO(#2716): Implement me!
-	return nil, errors.Errorf("Unimplemented -- GetCounter coming soon")
+	if counter, ok := a.counters[in.Name]; ok {
+		return counter, nil
+	}
+	return nil, errors.Errorf("NOT_FOUND. %s Counter not found", in.Name)
 }
 
-// UpdateCounter to be implemented
 func (a *alphaMock) UpdateCounter(ctx context.Context, in *alpha.UpdateCounterRequest, opts ...grpc.CallOption) (*alpha.Counter, error) {
-	// TODO(#2716): Implement me!
-	return nil, errors.Errorf("Unimplemented -- UpdateCounter coming soon")
+	counter, err := a.GetCounter(ctx, &alpha.GetCounterRequest{Name: in.CounterUpdateRequest.Name})
+	if err != nil {
+		return nil, err
+	}
+
+	switch {
+	case in.CounterUpdateRequest.CountDiff != 0:
+		count := counter.Count + in.CounterUpdateRequest.CountDiff
+		if count < 0 || count > counter.Capacity {
+			return nil, errors.Errorf("OUT_OF_RANGE. Count must be within range [0,Capacity]. Found Count: %d, Capacity: %d", count, counter.Capacity)
+		}
+		counter.Count = count
+	case in.CounterUpdateRequest.Count != nil:
+		countSet := in.CounterUpdateRequest.Count.GetValue()
+		if countSet < 0 || countSet > counter.Capacity {
+			return nil, errors.Errorf("OUT_OF_RANGE. Count must be within range [0,Capacity]. Found Count: %d, Capacity: %d", countSet, counter.Capacity)
+		}
+		counter.Count = countSet
+	case in.CounterUpdateRequest.Capacity != nil:
+		capacity := in.CounterUpdateRequest.Capacity.GetValue()
+		if capacity < 0 {
+			return nil, errors.Errorf("OUT_OF_RANGE. Capacity must be greater than or equal to 0. Found Capacity: %d", capacity)
+		}
+		counter.Capacity = capacity
+	default:
+		return nil, errors.Errorf("INVALID_ARGUMENT. Malformed CounterUpdateRequest: %v",
+			in.CounterUpdateRequest)
+	}
+
+	a.counters[in.CounterUpdateRequest.Name] = counter
+	return a.counters[in.CounterUpdateRequest.Name], nil
 }
 
 // GetList to be implemented
