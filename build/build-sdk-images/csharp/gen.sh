e484d1caa695f6c2ed8238d0a659103e47606cee
yoshd <garlic.ba.0129@gmail.com>
2023-09-22 01:36:01 +0900

    Change to use grpc-dotnet instead of Grpc.Core

--- build/build-sdk-images/csharp/gen.sh
+++ build/build-sdk-images/csharp/gen.sh
@@ -16,18 +16,15 @@
 
 set -ex
 
-header() {
-    cat /go/src/agones.dev/agones/build/boilerplate.go.txt "$1" | sponge "$1"
-}
-
-sdk=/go/src/agones.dev/agones/proto/sdk
-googleapis=/go/src/agones.dev/agones/proto/googleapis
+proto=/go/src/agones.dev/agones/proto
+sdk=${proto}/sdk
+googleapis=${proto}/googleapis
 protoc_intermediate=/go/src/agones.dev/agones/sdks/csharp/proto
-protoc_destination=/go/src/agones.dev/agones/sdks/csharp/sdk/generated
 
-# Create temporary proto files
+echo "Copying protobuffers to csharp sdk"
 mkdir -p ${protoc_intermediate}
 cp -r ${sdk} ${protoc_intermediate}
+cp -r ${googleapis} ${protoc_intermediate}
 
 # Remove protoc-gen-openapiv2 definitions because C# package doesn't support grpc-gateway
 sed -i -e 's/import "protoc-gen-openapiv2\/options\/annotations.proto";//' ${protoc_intermediate}/sdk/sdk.proto
@@ -39,6 +36,7 @@ sed -i -e 's/schemes: HTTP;//' ${protoc_intermediate}/sdk/sdk.proto
 sed -i -e 's/consumes: "application\/json";//' ${protoc_intermediate}/sdk/sdk.proto
 sed -i -z 's/produces: "application\/json";\n};//' ${protoc_intermediate}/sdk/sdk.proto
 sed -i -e 's/bool disabled = 1.*/bool disabled = 1;/' ${protoc_intermediate}/sdk/sdk.proto
+sed -i -e 's/^ *$//' ${protoc_intermediate}/sdk/sdk.proto
 
 sed -i -e 's/import "protoc-gen-openapiv2\/options\/annotations.proto";//' ${protoc_intermediate}/sdk/alpha/alpha.proto
 sed -i -e 's/option (grpc.gateway.protoc_gen_openapiv2.options.openapiv2_swagger) = {//' ${protoc_intermediate}/sdk/alpha/alpha.proto
@@ -49,19 +47,6 @@ sed -i -e 's/schemes: HTTP;//' ${protoc_intermediate}/sdk/alpha/alpha.proto
 sed -i -e 's/consumes: "application\/json";//' ${protoc_intermediate}/sdk/alpha/alpha.proto
 sed -i -z 's/produces: "application\/json";\n};//' ${protoc_intermediate}/sdk/alpha/alpha.proto
 sed -i -e 's/bool bool = 1.*/bool bool = 1;/' ${protoc_intermediate}/sdk/alpha/alpha.proto
+sed -i -e 's/^ *$//' ${protoc_intermediate}/sdk/alpha/alpha.proto
 
-# Generate C# proto file like `Sdk.cs`
-protoc -I ${googleapis} -I ${protoc_intermediate}/sdk --plugin=protoc-gen-grpc=`which grpc_csharp_plugin` --csharp_out=${protoc_destination}  ${protoc_intermediate}/sdk/sdk.proto
-protoc -I ${googleapis} -I ${protoc_intermediate}/sdk/alpha --plugin=protoc-gen-grpc=`which grpc_csharp_plugin` --csharp_out=${protoc_destination} ${protoc_intermediate}/sdk/alpha/alpha.proto
-
-# Generate grpc file like `SdkGrpc.cs`
-protoc -I ${googleapis} -I ${protoc_intermediate}/sdk --plugin=protoc-gen-grpc=`which grpc_csharp_plugin` --grpc_out=${protoc_destination} ${protoc_intermediate}/sdk/sdk.proto
-protoc -I ${googleapis} -I ${protoc_intermediate}/sdk/alpha --plugin=protoc-gen-grpc=`which grpc_csharp_plugin`  --grpc_out=${protoc_destination} ${protoc_intermediate}/sdk/alpha/alpha.proto
-
-cd ${protoc_destination}
-header Alpha.cs
-header AlphaGrpc.cs
-header Sdk.cs
-header SdkGrpc.cs
-
-rm -rf ${protoc_intermediate}
+echo "csharp code is generated at build time"
