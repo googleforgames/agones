d1660e0e5ae1ed4d6463d52f7d8bedd3f48ac7ea
yoshd <garlic.ba.0129@gmail.com>
2023-09-22 21:35:38 +0900

    Move proto file output

--- build/build-sdk-images/csharp/gen.sh
+++ build/build-sdk-images/csharp/gen.sh
@@ -19,34 +19,34 @@ set -ex
 proto=/go/src/agones.dev/agones/proto
 sdk=${proto}/sdk
 googleapis=${proto}/googleapis
-protoc_intermediate=/go/src/agones.dev/agones/sdks/csharp/proto
+csharp_proto_file_output_dir=/go/src/agones.dev/agones/sdks/csharp/sdk/proto
 
 echo "Copying protobuffers to csharp sdk"
-mkdir -p ${protoc_intermediate}
-cp -r ${sdk} ${protoc_intermediate}
-cp -r ${googleapis} ${protoc_intermediate}
+mkdir -p ${csharp_proto_file_output_dir}
+cp -r ${sdk} ${csharp_proto_file_output_dir}
+cp -r ${googleapis} ${csharp_proto_file_output_dir}
 
 # Remove protoc-gen-openapiv2 definitions because C# package doesn't support grpc-gateway
-sed -i -e 's/import "protoc-gen-openapiv2\/options\/annotations.proto";//' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -e 's/option (grpc.gateway.protoc_gen_openapiv2.options.openapiv2_swagger) = {//' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -e 's/info: {//' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -e 's/title: "sdk.proto";//' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -z 's/version: "version not set";\n    };//' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -e 's/schemes: HTTP;//' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -e 's/consumes: "application\/json";//' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -z 's/produces: "application\/json";\n};//' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -e 's/bool disabled = 1.*/bool disabled = 1;/' ${protoc_intermediate}/sdk/sdk.proto
-sed -i -e 's/^ *$//' ${protoc_intermediate}/sdk/sdk.proto
+sed -i -e 's/import "protoc-gen-openapiv2\/options\/annotations.proto";//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -e 's/option (grpc.gateway.protoc_gen_openapiv2.options.openapiv2_swagger) = {//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -e 's/info: {//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -e 's/title: "sdk.proto";//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -z 's/version: "version not set";\n    };//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -e 's/schemes: HTTP;//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -e 's/consumes: "application\/json";//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -z 's/produces: "application\/json";\n};//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -e 's/bool disabled = 1.*/bool disabled = 1;/' ${csharp_proto_file_output_dir}/sdk/sdk.proto
+sed -i -e 's/^ *$//' ${csharp_proto_file_output_dir}/sdk/sdk.proto
 
-sed -i -e 's/import "protoc-gen-openapiv2\/options\/annotations.proto";//' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -e 's/option (grpc.gateway.protoc_gen_openapiv2.options.openapiv2_swagger) = {//' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -e 's/info: {//' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -e 's/title: "alpha.proto";//' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -z 's/version: "version not set";\n    };//' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -e 's/schemes: HTTP;//' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -e 's/consumes: "application\/json";//' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -z 's/produces: "application\/json";\n};//' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -e 's/bool bool = 1.*/bool bool = 1;/' ${protoc_intermediate}/sdk/alpha/alpha.proto
-sed -i -e 's/^ *$//' ${protoc_intermediate}/sdk/alpha/alpha.proto
+sed -i -e 's/import "protoc-gen-openapiv2\/options\/annotations.proto";//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -e 's/option (grpc.gateway.protoc_gen_openapiv2.options.openapiv2_swagger) = {//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -e 's/info: {//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -e 's/title: "alpha.proto";//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -z 's/version: "version not set";\n    };//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -e 's/schemes: HTTP;//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -e 's/consumes: "application\/json";//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -z 's/produces: "application\/json";\n};//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -e 's/bool bool = 1.*/bool bool = 1;/' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
+sed -i -e 's/^ *$//' ${csharp_proto_file_output_dir}/sdk/alpha/alpha.proto
 
 echo "csharp code is generated at build time"
