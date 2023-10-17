d3894b70cd78cbce27e0442247845e05a225e136
gongmax <gongmax@google.com>
2023-10-17 22:58:12 +0000

    fix

--- build/includes/build-image.mk
+++ build/includes/build-image.mk
@@ -51,6 +51,10 @@ endif
 pull-build-image:
 	$(MAKE) pull-remote-build-image REMOTE_TAG=$(build_remote_tag) LOCAL_TAG=$(build_tag)
 
+pull-build-image-with-tag:
+	echo $(CUSTOM_LOCAL_TAG)
+	$(MAKE) pull-remote-build-image REMOTE_TAG=$(build_remote_tag) LOCAL_TAG=$(CUSTOM_LOCAL_TAG)
+
 # push the local build image up to your repository
 push-build-image:
 	$(MAKE) push-remote-build-image REMOTE_TAG=$(build_remote_tag) LOCAL_TAG=$(build_tag)
