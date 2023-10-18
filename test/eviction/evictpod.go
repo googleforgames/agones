f9b064a0e63b52ee0555e55c9374c59a418e7ed3
Zach Loafman <zml@google.com>
2023-10-18 16:06:50 +0000

    Fix review comments

--- test/eviction/evictpod.go
+++ test/eviction/evictpod.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License
 
-// evictpod.go --pod <pod> --namespace <namespae> initiates a pod eviction
+// evictpod.go --pod <pod> --namespace <namespace> initiates a pod eviction
 // using the k8s eviction API.
 package main
 
@@ -22,7 +22,7 @@ import (
 	"log"
 	"path/filepath"
 
-	policy "k8s.io/api/policy/v1beta1"
+	policy "k8s.io/api/policy/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -31,7 +31,7 @@ import (
 
 // Borrowed from https://stackoverflow.com/questions/62803041/how-to-evict-or-delete-pods-from-kubernetes-using-golang-client
 func evictPod(client *kubernetes.Clientset, name, namespace string) error {
-	return client.PolicyV1beta1().Evictions(namespace).Evict(context.TODO(), &policy.Eviction{
+	return client.PolicyV1().Evictions(namespace).Evict(context.TODO(), &policy.Eviction{
 		ObjectMeta: meta_v1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
@@ -40,27 +40,25 @@ func evictPod(client *kubernetes.Clientset, name, namespace string) error {
 
 func main() {
 	kubeconfig := flag.String("kubeconfig", filepath.Join(homedir.HomeDir(), ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	namespace := flag.String("namespace", "default", "Namespace (required)")
+	namespace := flag.String("namespace", "default", "Namespace (defaults to `default`)")
 	pod := flag.String("pod", "", "Pod name (required)")
 	flag.Parse()
 
-	if *pod == "" || *namespace == "" {
-		log.Fatal("--pod and --namespace must be non-empty")
+	if *pod == "" {
+		log.Fatal("--pod must be non-empty")
 	}
 
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		log.Fatalf("Could not create in cluster config: %v", err)
+		log.Fatalf("Could not build config: %v", err)
 	}
 
-	var kubeClient *kubernetes.Clientset
-	kubeClient, err = kubernetes.NewForConfig(config)
+	kubeClient, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Fatalf("Could not create the kubernetes clientset: %v", err)
 	}
 
-	err = evictPod(kubeClient, *pod, *namespace)
-	if err != nil {
+	if err = evictPod(kubeClient, *pod, *namespace); err != nil {
 		log.Fatalf("Pod eviction failed: %v", err)
 	}
 }
