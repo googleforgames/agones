8238032fed376e72e9ebc74dafe2fc7dde8b144c
Mark Mandel <markmandel@google.com>
2023-10-31 11:47:30 -0700

    Fixes for generated content drift
    * Include the gRPC version in the `build_sdk_base_version` version such
    that it automatically gets rebuilt whenever the gRPC version changes
    without manual intervention.
    
    Also updated the changes in the allocation gRPC code as well.

--- build/includes/sdk.mk
+++ build/includes/sdk.mk
@@ -20,14 +20,15 @@
 #   \__, |_| \_\_|    \____|   |_|\___/ \___/|_|_|_| |_|\__, |
 #   |___/                                               |___/
 
-build_sdk_base_version = $(call sha,$(build_path)/build-sdk-images/tool/base/Dockerfile)
+grpc_release_tag = v1.57.0
+
+build_sdk_base_version = $(call sha,$(build_path)/build-sdk-images/tool/base/Dockerfile)_$(grpc_release_tag)
 build_sdk_base_tag = agones-build-sdk-base:$(build_sdk_base_version)
 
 # Calculate sha hash of sha hashes of all files in a specified SDK_FOLDER
 build_sdk_version = $(call sha_dir,$(build_path)/build-sdk-images/$(SDK_FOLDER)/*)
 build_sdk_base_remote_tag = $(REGISTRY)/$(build_sdk_base_tag)
 build_sdk_prefix = agones-build-sdk-
-grpc_release_tag = v1.57.0
 sdk_build_folder = build-sdk-images/
 examples_folder = ../examples/
 SDK_FOLDER ?= go
