2cf0a1cb9e0ff6c2ebbc88459d7e026543282961
Ivy Gooch <igooch@google.com>
2023-11-14 00:11:51 +0000

    Bumps simple-game-server version to 0.22

--- test/e2e/gameserver_test.go
+++ test/e2e/gameserver_test.go
@@ -1000,7 +1000,7 @@ spec:
           preferredDuringSchedulingIgnoredDuringExecution: ERROR
       containers:
         - name: simple-game-server
-          image: us-docker.pkg.dev/agones-images/examples/simple-game-server:0.21
+          image: us-docker.pkg.dev/agones-images/examples/simple-game-server:0.22
 `
 	err := os.WriteFile("/tmp/invalid.yaml", []byte(gsYaml), 0o644)
 	require.NoError(t, err)
