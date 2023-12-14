e666904de574a95a6a504399370d66e339bfa05f
Kalaiselvim <117940852+Kalaiselvi84@users.noreply.github.com>
2023-12-14 21:22:13 +0000

    Bumps simple-game-server to 0.23 (#3543)

--- pkg/util/webhooks/webhooks_test.go
+++ pkg/util/webhooks/webhooks_test.go
@@ -163,7 +163,7 @@ func TestWebHookFleetValidationHandler(t *testing.T) {
 							"template": {
 								"spec": {
 									"containers": [{
-										"image": "us-docker.pkg.dev/agones-images/examples/simple-game-server:0.22",
+										"image": "us-docker.pkg.dev/agones-images/examples/simple-game-server:0.23",
 										"name": false
 									}]
 								}
