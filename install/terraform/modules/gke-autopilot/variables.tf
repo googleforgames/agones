a193a5ae5c8bdb739677024e1bed318549a136b4
Mengye (Max) Gong <8364575+gongmax@users.noreply.github.com>
2023-12-15 18:15:26 -0800

    Disable deletion protection for Autopilot test clusters (#3468)
    * Disable deletion protection for Autopilot test clusters

--- install/terraform/modules/gke-autopilot/variables.tf
+++ install/terraform/modules/gke-autopilot/variables.tf
@@ -20,13 +20,16 @@ variable "cluster" {
   type        = map(any)
 
   default = {
-    "name"              = "test-cluster"
-    "project"           = "agones"
-    "location"          = "us-west1"
-    "network"           = "default"
-    "subnetwork"        = ""
-    "releaseChannel"    = "REGULAR"
-    "kubernetesVersion" = "1.27"
+    "name"                          = "test-cluster"
+    "project"                       = "agones"
+    "location"                      = "us-west1"
+    "network"                       = "default"
+    "subnetwork"                    = ""
+    "releaseChannel"                = "REGULAR"
+    "kubernetesVersion"             = "1.27"
+    "deletionProtection"            = true
+    "maintenanceExclusionStartTime" = null
+    "maintenanceExclusionEndTime"   = null
   }
 }
 
