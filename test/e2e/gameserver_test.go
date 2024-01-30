b591f8fc85b85546b8c8a0b600c62aa8a24fcc13
Kalaiselvi Murugesan <kalaiselvim@google.com>
2024-01-30 05:55:14 +0000

    bump simple-game-server

--- test/e2e/gameserver_test.go
+++ test/e2e/gameserver_test.go
@@ -1100,7 +1100,7 @@ spec:
           preferredDuringSchedulingIgnoredDuringExecution: ERROR
       containers:
         - name: simple-game-server
-          image: us-docker.pkg.dev/agones-images/examples/simple-game-server:0.26
+          image: us-docker.pkg.dev/agones-images/examples/simple-game-server:0.27
 `
 	err := os.WriteFile("/tmp/invalid.yaml", []byte(gsYaml), 0o644)
 	require.NoError(t, err)
