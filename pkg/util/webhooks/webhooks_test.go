4570f0fa1088455537ecc18a0ec6ce9f36ab5bad
Kalaiselvi Murugesan <kalaiselvim@google.com>
2024-01-30 00:46:20 +0000

    bump simple-game-server image outside example/simple-game-server

--- pkg/util/webhooks/webhooks_test.go
+++ pkg/util/webhooks/webhooks_test.go
@@ -164,7 +164,7 @@ func TestWebHookFleetValidationHandler(t *testing.T) {
 							"template": {
 								"spec": {
 									"containers": [{
-										"image": "us-docker.pkg.dev/agones-images/examples/simple-game-server:0.25",
+										"image": "us-docker.pkg.dev/agones-images/examples/simple-game-server:0.26",
 										"name": false
 									}]
 								}
