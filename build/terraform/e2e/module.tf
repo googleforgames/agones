c95f5917423a71bcaf8e55b3b85fdaacfe8ff887
Zach Loafman <zml@google.com>
2024-03-19 07:34:47 -0700

    Be prescriptive about rotating regions when updating Kubernetes versions (#3716)
    * I ensured quota and added alerts in `us-east1`, `us-west1`,
    `europe-west1`, and `asia-east1` to ensure we have 4 regions outside
    of `us-central1` that we can use for e2es. (`us-central1` is where
    we're running perf tests, so I'd rather isolate it.)
    
    * In this PR, I added instructions on how to cycle through the e2e
    regions as we bump versions, ensuring we stay in these regions and
    don't duplicate regions in 1.{N-3} .. 1.{N}.
    
    Co-authored-by: Kalaiselvim <117940852+Kalaiselvi84@users.noreply.github.com>

--- build/terraform/e2e/module.tf
+++ build/terraform/e2e/module.tf
@@ -41,6 +41,24 @@ variable "kubernetes_versions" {
     "1.27" = ["us-east1", "RAPID"]
     "1.28" = ["us-west1", "RAPID"]
     "1.29" = ["europe-west1", "RAPID"]
+    // "1.30" = ["asia-east1", "RAPID"]
+    //
+    // Before merge: When adding Kubernetes version 1.{N}, first uncomment the line above, extending
+    // the infrastructure to 4 versions temporarily. Come back to these instructions after the
+    // update PR merges.
+    //
+    // After merge: After the Kubernetes update PR merges, and all active PRs are updated:
+    //
+    // * Move the 1.{N-3} line to the bottom and comment it out
+    // * Change the (commented out) 1.{N-3} to 1.{N+1}
+    // * You should now have 3 versions uncommented (versions 1.{N-2} .. 1.{N}),
+    //   and 1.{N+1} commented out for the next update. The new, commented out 1.{N+1}
+    //   should be using the region of the previous 1.{N-3} - this region will become
+    //   unused.
+    //
+    // Rationale: We cycle the regions us-east1 -> us-west1 -> europe-west1 -> asia-east1 -> us-east1
+    // as versions are added, using 4 regions so that the PR adding 1.{N} is in a unique region to
+    // 1.{N-3} .. 1.{N-1}, meaning versions never need to share a region in CI.
   }
 }
 
