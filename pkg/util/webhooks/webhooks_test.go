4ff3973ca985ed641a21a6578d0417c6d6babfa1
Kalaiselvi Murugesan <kalaiselvim@google.com>
2024-04-19 17:57:40 +0000

    bump sgs:0.30

--- pkg/util/webhooks/webhooks_test.go
+++ pkg/util/webhooks/webhooks_test.go
@@ -164,7 +164,7 @@ func TestWebHookFleetValidationHandler(t *testing.T) {
 							"template": {
 								"spec": {
 									"containers": [{
-										"image": "us-docker.pkg.dev/agones-images/examples/simple-game-server:0.29",
+										"image": "us-docker.pkg.dev/agones-images/examples/simple-game-server:0.30",
 										"name": false
 									}]
 								}
