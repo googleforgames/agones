454a6034ee64fc49a303045aec314a7c8de84dff
Kalaiselvi Murugesan <kalaiselvim@google.com>
2024-04-20 01:17:33 +0000

    bump xonotic

--- test/e2e/examples_test.go
+++ test/e2e/examples_test.go
@@ -203,7 +203,7 @@ func TestXonoticGameServerReady(t *testing.T) {
 					Containers: []corev1.Container{
 						{
 							Name:  "xonotic",
-							Image: "us-docker.pkg.dev/agones-images/examples/xonotic-example:1.8",
+							Image: "us-docker.pkg.dev/agones-images/examples/xonotic-example:1.9",
 							Resources: corev1.ResourceRequirements{
 								Requests: corev1.ResourceList{
 									corev1.ResourceMemory: resource.MustParse("700Mi"),
