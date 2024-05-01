286746049f64c3c8c163eb99f6ebe4ae46c1674b
Kalaiselvi Murugesan <kalaiselvim@google.com>
2024-05-01 02:44:29 +0000

    Beta in tests, remove space in gen.sh, cleanup alpha & beta.proto

--- build/build-sdk-images/csharp/gen.sh
+++ build/build-sdk-images/csharp/gen.sh
@@ -53,7 +53,7 @@ sed -i -e 's/import "protoc-gen-openapiv2\/options\/annotations.proto";//' ${csh
 sed -i -e 's/option (grpc.gateway.protoc_gen_openapiv2.options.openapiv2_swagger) = {//' ${csharp_proto_file_output_dir}/sdk/beta/beta.proto
 sed -i -e 's/info: {//' ${csharp_proto_file_output_dir}/sdk/beta/beta.proto
 sed -i -e 's/title: "beta.proto";//' ${csharp_proto_file_output_dir}/sdk/beta/beta.proto
-sed -i -z 's/version: "version not set";\n    };//' ${csharp_proto_file_output_dir}/sdk/beta/beta.proto
+sed -i -z 's/version: "version not set";\n  };//' ${csharp_proto_file_output_dir}/sdk/beta/beta.proto
 sed -i -e 's/schemes: HTTP;//' ${csharp_proto_file_output_dir}/sdk/beta/beta.proto
 sed -i -e 's/consumes: "application\/json";//' ${csharp_proto_file_output_dir}/sdk/beta/beta.proto
 sed -i -z 's/produces: "application\/json";\n};//' ${csharp_proto_file_output_dir}/sdk/beta/beta.proto
--- sdks/csharp/test/AgonesBetaSDKClientTests.cs
+++ sdks/csharp/test/AgonesBetaSDKClientTests.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-\using Agones.Dev.Sdk.Beta;
+using Agones.Dev.Sdk.Beta;
 using Grpc.Core;
 using System.Collections.Generic;
 using System.Linq;
