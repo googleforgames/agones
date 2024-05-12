c8979d832c2ea3448b947c96addaed86892774c1
steven-supersolid <steven@supersolid.com>
2024-05-12 18:14:30 +0100

    update conformance test

--- test/sdk/nodejs/testSDKClient.js
+++ test/sdk/nodejs/testSDKClient.js
@@ -66,4 +66,95 @@ const connect = async () => {
 	}
 };
 
+const testCounts = async(sdk) {
+	// LocalSDKServer starting "rooms": {Count: 1, Capacity: 10}
+	const counter = "rooms";
+
+	try {
+		let count = await sdk.alpha.getCounterCount(counter);
+		if (count !== 1) {
+			throw new Error(`Counter count should be 1, but is ${count}`);
+		}
+	} catch (error) {
+		throw new Error(`Error getting Counter count: ${error}`);
+	}
+
+	try {
+		await sdk.alpha.incrementCounter(counter, 9);
+	} catch (error) {
+		throw new Error(`Error incrementing Counter: ${error}`);
+	}
+
+	try {
+		await sdk.alpha.decrementCounter(counter, 10);
+	} catch (error) {
+		throw new Error(`Error decrementing Counter: ${error}`);
+	}
+
+	try {
+		await sdk.alpha.setCounterCount(counter, 10);
+	} catch (error) {
+		throw new Error(`Error setting Counter count: ${error}`);
+	}
+
+	try {
+		let capacity = await sdk.alpha.getCounterCapacity(counter);
+		if (capacity !== 10) {
+			throw new Error(`Counter capacity should be 10, but is ${capacity}`);
+		}
+	} catch (error) {
+		throw new Error(`Error getting Counter capacity: ${error}`);
+	}
+
+	try {
+		await sdk.alpha.setCounterCapacity(counter, 1);
+	} catch (error) {
+		throw new Error(`Error setting Counter capacity: ${error}`);
+	}
+}
+
+const testLists = async(sdk) {
+	// LocalSDKServer starting "players": {Values: []string{"test0", "test1", "test2"}, Capacity: 100}}
+	const list = "players"
+	const listValues = ["test0", "test1", "test2"]
+
+	let contains = await sdk.alpha.listContains(list, "test1");
+	if (!contains) {
+		throw new Error("List should contain value \"test1\"");
+	}
+
+	try {
+		let length = await sdk.alpha.getListLength(list);
+		if (length !== 3) {
+			throw new Error(`List length should be 3, but is ${length}`);
+		}
+	} catch (error) {
+		throw new Error(`Error getting List length: ${error}`);
+	}
+
+	try {
+		let values = await sdk.alpha.getListValues(list);
+		if (JSON.stringify(values) !== JSON.stringify(listValues)) {
+			throw new Error(`List values should be ${listValues}, but is ${values}`);
+		}
+	} catch (error) {
+		throw new Error(`Error getting List values: ${error}`);
+	}
+
+	await sdk.alpha.appendListValue(list, "test3");
+
+	await sdk.alpha.deleteListValue(list, "test2");
+
+	try {
+		let capacity = await sdk.alpha.getListCapacity(list);
+		if (capacity !== 100) {
+			throw new Error(`List capacity should be 100, but is ${capacity}`);
+		}
+	} catch (error) {
+		throw new Error(`Error getting List capacity: ${error}`);
+	}
+
+	await sdk.alpha.setListCapacity(list, 2);
+}
+
 connect();
