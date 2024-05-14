6be9de0d5fcef1e8921f138a997aaf5c34d5848f
thomaskeakie <thomas.lacroix@ubisoft.com>
2024-05-13 20:54:52 -0400

    Generate grpc for nodejs from alpha to beta

--- build/build-sdk-images/node/gen.sh
+++ build/build-sdk-images/node/gen.sh
@@ -24,15 +24,18 @@ sdk=/go/src/agones.dev/agones/proto/sdk
 googleapis=/go/src/agones.dev/agones/proto/googleapis
 gatewaygrpc=/go/src/agones.dev/agones/proto/grpc-gateway
 
-mkdir -p ./sdks/nodejs/lib/alpha
-
 cd /go/src/agones.dev/agones
+
+mkdir -p ./sdks/nodejs/lib/alpha
+mkdir -p ./sdks/nodejs/lib/beta
 
 grpc_tools_node_protoc -I ${googleapis} -I ${gatewaygrpc} -I ${sdk} --js_out=import_style=commonjs,binary:./sdks/nodejs/lib google/api/annotations.proto google/api/client.proto google/api/field_behavior.proto google/api/http.proto google/api/launch_stage.proto google/api/resource.proto protoc-gen-openapiv2/options/annotations.proto protoc-gen-openapiv2/options/openapiv2.proto
 grpc_tools_node_protoc -I ${googleapis} -I ${gatewaygrpc} -I ${sdk}/alpha --js_out=import_style=commonjs,binary:./sdks/nodejs/lib/alpha google/api/annotations.proto google/api/client.proto google/api/field_behavior.proto google/api/http.proto google/api/launch_stage.proto google/api/resource.proto protoc-gen-openapiv2/options/annotations.proto protoc-gen-openapiv2/options/openapiv2.proto
+grpc_tools_node_protoc -I ${googleapis} -I ${gatewaygrpc} -I ${sdk}/beta --js_out=import_style=commonjs,binary:./sdks/nodejs/lib/beta google/api/annotations.proto google/api/client.proto google/api/field_behavior.proto google/api/http.proto google/api/launch_stage.proto google/api/resource.proto protoc-gen-openapiv2/options/annotations.proto protoc-gen-openapiv2/options/openapiv2.proto
 
 grpc_tools_node_protoc -I ${googleapis} -I ${gatewaygrpc} -I ${sdk} --grpc_out=generate_package_definition:./sdks/nodejs/lib --js_out=import_style=commonjs,binary:./sdks/nodejs/lib sdk.proto
 grpc_tools_node_protoc -I ${googleapis} -I ${gatewaygrpc} -I ${sdk}/alpha --grpc_out=generate_package_definition:./sdks/nodejs/lib/alpha --js_out=import_style=commonjs,binary:./sdks/nodejs/lib/alpha alpha.proto
+grpc_tools_node_protoc -I ${googleapis} -I ${gatewaygrpc} -I ${sdk}/beta --grpc_out=generate_package_definition:./sdks/nodejs/lib/beta --js_out=import_style=commonjs,binary:./sdks/nodejs/lib/beta beta.proto
 
 header ./sdks/nodejs/lib/sdk_pb.js
 header ./sdks/nodejs/lib/sdk_grpc_pb.js
@@ -47,3 +50,10 @@ header ./sdks/nodejs/lib/alpha/google/api/annotations_pb.js
 header ./sdks/nodejs/lib/alpha/google/api/http_pb.js
 header ./sdks/nodejs/lib/alpha/protoc-gen-openapiv2/options/annotations_pb.js
 header ./sdks/nodejs/lib/alpha/protoc-gen-openapiv2/options/openapiv2_pb.js
+
+header ./sdks/nodejs/lib/beta/beta_pb.js
+header ./sdks/nodejs/lib/beta/beta_grpc_pb.js
+header ./sdks/nodejs/lib/beta/google/api/annotations_pb.js
+header ./sdks/nodejs/lib/beta/google/api/http_pb.js
+header ./sdks/nodejs/lib/beta/protoc-gen-openapiv2/options/annotations_pb.js
+header ./sdks/nodejs/lib/beta/protoc-gen-openapiv2/options/openapiv2_pb.js
