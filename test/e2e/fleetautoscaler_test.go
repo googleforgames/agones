33dcddcc289957e431c706d13c64eea6d0795e02
Ashutosh Singh <ashutoshnsingh@google.com>
2024-07-10 10:33:42 +0000

    updated go version with images

--- test/e2e/fleetautoscaler_test.go
+++ test/e2e/fleetautoscaler_test.go
@@ -645,7 +645,7 @@ func defaultAutoscalerWebhook(namespace string) (*corev1.Pod, *corev1.Service) {
 		},
 		Spec: corev1.PodSpec{
 			Containers: []corev1.Container{{Name: "webhook",
-				Image:           "us-docker.pkg.dev/agones-images/examples/autoscaler-webhook:0.13",
+				Image:           "us-docker.pkg.dev/agones-images/examples/autoscaler-webhook:0.14",
 				ImagePullPolicy: corev1.PullAlways,
 				Ports: []corev1.ContainerPort{{
 					ContainerPort: 8000,
