e95d3c13727a5ddd4419e5df938221a2cb176efc
Ivy Gooch <igooch@google.com>
2024-07-10 02:08:51 +0000

    Adds a command to generate the zz_generated.deepcopy.go files for the apis

---
+++ build/build-image/gen-crd-helpers.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Copyright 2024 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+set -x
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CODEGEN_SCRIPT="/go/src/k8s.io/code-generator/kube_codegen.sh"
+
+source "${CODEGEN_SCRIPT}"
+
+echo "Generating CRD conversions, deepcopy, and defaults code..."
+
+kube::codegen::gen_helpers \
+  --boilerplate /go/src/agones.dev/agones/build/boilerplate.go.txt \
+  /go/src/agones.dev/agones/pkg/apis
+
+echo "CRD conversions, deepcopy, and defaults code generation complete."
