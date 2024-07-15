74e8d74240d5dadfc5766824a73745dd468d0508
ashutosji <108933935+ashutosji@users.noreply.github.com>
2024-07-15 18:35:08 +0000

    update go version to 1.21.12 (#3894)
    * update go version
    
    * updated go version with images
    
    * updated each images in prod
    
    * Adds CloudBuild for simple-genai example

--- pkg/util/webhooks/webhooks_test.go
+++ pkg/util/webhooks/webhooks_test.go
@@ -164,7 +164,7 @@ func TestWebHookFleetValidationHandler(t *testing.T) {
 							"template": {
 								"spec": {
 									"containers": [{
-										"image": "us-docker.pkg.dev/agones-images/examples/simple-game-server:0.33",
+										"image": "us-docker.pkg.dev/agones-images/examples/simple-game-server:0.34",
 										"name": false
 									}]
 								}
