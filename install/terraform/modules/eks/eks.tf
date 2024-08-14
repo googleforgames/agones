3fa5ab740007d176b019f556a4228fb8a1b9faf9
ashutosji <108933935+ashutosji@users.noreply.github.com>
2024-08-14 16:29:56 +0000

    Update Supported Kubernetes to 1.28, 1.29, 1.30 (#3933)
    Co-authored-by: Mengye (Max) Gong <8364575+gongmax@users.noreply.github.com>

--- install/terraform/modules/eks/eks.tf
+++ install/terraform/modules/eks/eks.tf
@@ -100,7 +100,7 @@ module "eks" {
   cluster_name    = var.cluster_name
   subnets         = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
-  cluster_version = "1.29"
+  cluster_version = "1.30"
 
   worker_groups_launch_template = [
     {
