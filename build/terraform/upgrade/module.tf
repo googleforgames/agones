dd7b0e76ef426313415c870d3b40e26697c460bf
igooch <igooch@google.com>
2024-11-21 13:25:24 -0800

    Changes upgrades clusters to use only us based regions (#4046)

--- build/terraform/upgrade/module.tf
+++ build/terraform/upgrade/module.tf
@@ -38,14 +38,13 @@ variable "kubernetes_versions" {
   description = "Create upgrade test clusters with these k8s versions in these regions"
   type        = map(list(string))
   default     = {
-    "1.29" = ["europe-west1", "RAPID"]
-    "1.30" = ["asia-east1", "RAPID"]
+    "1.29" = ["us-west1", "RAPID"]
+    "1.30" = ["us-central1", "RAPID"]
     "1.31" = ["us-east1", "RAPID"]
     //1.32" = ["us-west1", "RAPID"]
     //
-    // Before merge: When adding Kubernetes version 1.{N}, first uncomment the line above, extending
-    // the infrastructure to 4 versions temporarily. Come back to these instructions after the
-    // update PR merges.
+    // Before merge: When adding Kubernetes version 1.{N}, first uncomment the line above. Come back
+    // to these instructions after the update PR merges.
     //
     // After merge: After the Kubernetes update PR merges, and all active PRs are updated:
     //
@@ -53,12 +52,11 @@ variable "kubernetes_versions" {
     // * Change the (commented out) 1.{N-3} to 1.{N+1}
     // * You should now have 3 versions uncommented (versions 1.{N-2} .. 1.{N}),
     //   and 1.{N+1} commented out for the next update. The new, commented out 1.{N+1}
-    //   should be using the region of the previous 1.{N-3} - this region will become
-    //   unused.
+    //   should be using the region of the current 1.{N-3}.
     //
-    // Rationale: We cycle the regions us-east1 -> us-west1 -> europe-west1 -> asia-east1 -> us-east1
-    // as versions are added, using 4 regions so that the PR adding 1.{N} is in a unique region to
-    // 1.{N-3} .. 1.{N-1}, meaning versions never need to share a region in CI.
+    // Rationale: We cycle the regions us-east1 -> us-west1 -> us-central1 -> us-east1 as versions
+    // are added, using 3 regions so that the PR adding 1.{N} is in a unique region to
+    // 1.{N-2} .. 1.{N-1}, meaning versions never need to share a region in CI.
   }
 }
 
