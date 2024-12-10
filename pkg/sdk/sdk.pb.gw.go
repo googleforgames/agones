f91758f8394178695b0f68c68e7a9462978ac621
Christian Reinecke <christian.reinecke@nitrado.net>
2024-12-10 14:57:08 +0100

    fix: rebase & build-sdks

--- pkg/sdk/sdk.pb.gw.go
+++ pkg/sdk/sdk.pb.gw.go
@@ -259,11 +259,7 @@ func request_SDK_SetAnnotations_0(ctx context.Context, marshaler runtime.Marshal
 	var protoReq KeyValues
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -276,11 +272,7 @@ func local_request_SDK_SetAnnotations_0(ctx context.Context, marshaler runtime.M
 	var protoReq KeyValues
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
