838ca62d8046580d63c94ab7849c4e09f36c8a1b
Thomas Lacroix <thomas.lacroix@ubisoft.com>
2025-07-25 11:59:15 -0400

    feat: add lease config

--- cmd/processor/main.go
+++ cmd/processor/main.go
@@ -42,6 +42,9 @@ const (
 	logLevelFlag       = "log-level"
 	leaderElectionFlag = "leader-election"
 	podNamespace       = "pod-namespace"
+	leaseDurationFlag  = "lease-duration"
+	renewDeadlineFlag  = "renew-deadline"
+	retryPeriodFlag    = "retry-period"
 )
 
 var (
@@ -52,16 +55,25 @@ type processorConfig struct {
 	LogLevel       string
 	LeaderElection bool
 	PodNamespace   string
+	LeaseDuration  time.Duration
+	RenewDeadline  time.Duration
+	RetryPeriod    time.Duration
 }
 
 func parseEnvFlags() processorConfig {
 	viper.SetDefault(logLevelFlag, "Info")
 	viper.SetDefault(leaderElectionFlag, false)
 	viper.SetDefault(podNamespace, "")
+	viper.SetDefault(leaseDurationFlag, 15*time.Second)
+	viper.SetDefault(renewDeadlineFlag, 10*time.Second)
+	viper.SetDefault(retryPeriodFlag, 2*time.Second)
 
 	pflag.String(logLevelFlag, viper.GetString(logLevelFlag), "Log level")
 	pflag.Bool(leaderElectionFlag, viper.GetBool(leaderElectionFlag), "Enable leader election")
 	pflag.String(podNamespace, viper.GetString(podNamespace), "Pod namespace")
+	pflag.Duration(leaseDurationFlag, viper.GetDuration(leaseDurationFlag), "Leader election lease duration")
+	pflag.Duration(renewDeadlineFlag, viper.GetDuration(renewDeadlineFlag), "Leader election renew deadline")
+	pflag.Duration(retryPeriodFlag, viper.GetDuration(retryPeriodFlag), "Leader election retry period")
 	pflag.Parse()
 
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
@@ -72,6 +84,9 @@ func parseEnvFlags() processorConfig {
 		LogLevel:       viper.GetString(logLevelFlag),
 		LeaderElection: viper.GetBool(leaderElectionFlag),
 		PodNamespace:   viper.GetString(podNamespace),
+		LeaseDuration:  viper.GetDuration(leaseDurationFlag),
+		RenewDeadline:  viper.GetDuration(renewDeadlineFlag),
+		RetryPeriod:    viper.GetDuration(retryPeriodFlag),
 	}
 }
 
@@ -120,7 +135,7 @@ func main() {
 		os.Exit(0)
 	})
 
-	whenLeader(ctx, cancelCtx, logger, conf.LeaderElection, kubeClient, conf.PodNamespace, func(ctx context.Context) {
+	whenLeader(ctx, cancelCtx, logger, conf, kubeClient, func(ctx context.Context) {
 		logger.Info("Starting processor work as leader")
 
 		// Simulate processor work (to ensure the leader is working)
@@ -141,8 +156,9 @@ func main() {
 	logger.Info("Processor exited gracefully.")
 }
 
-func whenLeader(ctx context.Context, cancel context.CancelFunc, logger *logrus.Entry, doLeaderElection bool, kubeClient *kubernetes.Clientset, namespace string, start func(_ context.Context)) {
-	if !doLeaderElection {
+func whenLeader(ctx context.Context, cancel context.CancelFunc, logger *logrus.Entry,
+	conf processorConfig, kubeClient *kubernetes.Clientset, start func(_ context.Context)) {
+	if !conf.LeaderElection {
 		start(ctx)
 		return
 	}
@@ -152,7 +168,7 @@ func whenLeader(ctx context.Context, cancel context.CancelFunc, logger *logrus.E
 	lock := &resourcelock.LeaseLock{
 		LeaseMeta: metav1.ObjectMeta{
 			Name:      "agones-processor-lock",
-			Namespace: namespace,
+			Namespace: conf.PodNamespace,
 		},
 		Client: kubeClient.CoordinationV1(),
 		LockConfig: resourcelock.ResourceLockConfig{
@@ -171,9 +187,9 @@ func whenLeader(ctx context.Context, cancel context.CancelFunc, logger *logrus.E
 		// get elected before your background loop finished, violating
 		// the stated goal of the lease.
 		ReleaseOnCancel: true,
-		LeaseDuration:   15 * time.Second,
-		RenewDeadline:   10 * time.Second,
-		RetryPeriod:     2 * time.Second,
+		LeaseDuration:   conf.LeaseDuration,
+		RenewDeadline:   conf.RenewDeadline,
+		RetryPeriod:     conf.RetryPeriod,
 		Callbacks: leaderelection.LeaderCallbacks{
 			OnStartedLeading: start,
 			OnStoppedLeading: func() {
