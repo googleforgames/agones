f013824022f16d71b67c3e0399909a0245482fd3
Sivasankaran R <sivasankaranr@google.com>
2025-10-09 10:37:05 +0000

    Update Inital Delay Seconds

--- test/e2e/examples_test.go
+++ test/e2e/examples_test.go
@@ -45,6 +45,10 @@ func TestSuperTuxKartGameServerReady(t *testing.T) {
 				PortPolicy:    agonesv1.Dynamic,
 				Protocol:      corev1.ProtocolUDP,
 			}},
+			Health: agonesv1.Health{
+				InitialDelaySeconds: 180,
+				PeriodSeconds:       10,
+			},
 			Template: corev1.PodTemplateSpec{
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
