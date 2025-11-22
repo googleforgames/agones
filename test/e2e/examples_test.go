05148c930b5a33d95fd2a9bd01bdf1bf99da2cff
Mark Mandel <mark@compoundtheory.com>
2025-11-22 15:50:05 -0800

    test: update SuperTuxKart example image to version 0.20 in e2e tests
    Closes #4321

--- test/e2e/examples_test.go
+++ test/e2e/examples_test.go
@@ -54,7 +54,7 @@ func TestSuperTuxKartGameServerReady(t *testing.T) {
 					Containers: []corev1.Container{
 						{
 							Name:  "supertuxkart",
-							Image: "us-docker.pkg.dev/agones-images/examples/supertuxkart-example:0.19",
+							Image: "us-docker.pkg.dev/agones-images/examples/supertuxkart-example:0.20",
 							Resources: corev1.ResourceRequirements{
 								Requests: corev1.ResourceList{
 									corev1.ResourceMemory: resource.MustParse("1Gi"),
